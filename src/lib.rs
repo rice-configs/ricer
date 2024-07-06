cdaeb042fee5a1f5d318f29d83d31d552fedb94c
Jason Pena <jasonpena@awkless.com>
2024-07-06 15:46:17 -0600

    feat(lib): Setup internal library
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
+// SPDX-License-Identifier: GPL-2.0-or-later WITH GPL-CC-1.0
+
+//! Welcome to Ricer's internal API!
+//!
+//! TODO: Document future modules...
+
+// TODO: Put future modules here...
