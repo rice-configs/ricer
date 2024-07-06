2c0fd88c971ea4a04ac30851d194cd63b733a9b8
Jason Pena <jasonpena@awkless.com>
2024-07-06 15:46:04 -0600

    feat(main): Setup `main`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ src/ricer.rs
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
+// SPDX-License-Identifier: GPL-2.0-or-later WITH GPL-CC-1.0
+
+fn main() {
+    println!("Hello from Ricer!");
+}
