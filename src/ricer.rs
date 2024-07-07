0f090355c042ef7f3b2b0f5c2c71cfba7969d3a3
Jason Pena <jasonpena@awkless.com>
2024-07-07 15:20:06 -0600

    feat(main)!: Setup `main` to use `rally::cli::Cli`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/ricer.rs
+++ src/ricer.rs
@@ -1,6 +1,21 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: GPL-2.0-or-later WITH GPL-CC-1.0
 
-fn main() {
-    println!("Hello from Ricer!");
+use log::{error, info};
+use std::process::ExitCode;
+
+use ricer::cli::Cli;
+
+fn main() -> ExitCode {
+    match Cli::new_run() {
+        Ok(out) => {
+            info!("{}", out);
+            ExitCode::SUCCESS
+        }
+
+        Err(error) => {
+            error!("{}", error);
+            ExitCode::FAILURE
+        }
+    }
 }
