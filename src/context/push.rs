7db66a7a6168efd065049f469a1c5cf58905bb9b
Jason Pena <jasonpena@awkless.com>
2024-07-10 15:17:44 -0600

    feat(ricer_core::context): Setup context for push command
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ src/context/push.rs
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
+// SPDX-License-Identifier: GPL-2.0-or-later WITH GPL-CC-1.0
+
+use crate::cli::{CommandSet, RicerCli, RunHookOpts};
+
+/// Context state for push command.
+#[derive(Debug)]
+pub struct PushContext {
+    /// Target remote to push too.
+    pub remote: Option<String>,
+
+    /// Target branch to push too.
+    pub branch: Option<String>,
+
+    /// Action to take when executing a hook specific to this command.
+    pub run_cmd_hook: RunHookOpts,
+
+    /// Action to take when executing a repository hook.
+    pub run_repo_hook: RunHookOpts,
+}
+
+impl From<RicerCli> for PushContext {
+    fn from(opts: RicerCli) -> Self {
+        let RicerCli {cmd_opts, cmd_set, ..} = opts;
+
+        let cmd_set = match cmd_set {
+            CommandSet::Push(opts) => opts,
+            _ => unreachable!("This should never happen. The command is not 'push'!"),
+        };
+
+        Self {
+            remote: cmd_set.remote,
+            branch: cmd_set.branch,
+            run_cmd_hook: cmd_opts.run_cmd_hook,
+            run_repo_hook: cmd_opts.run_repo_hook,
+        }
+    }
+}
