ddc72b725132b4c40af25a283b2c8d27780f5093
Jason Pena <jasonpena@awkless.com>
2024-07-09 22:53:59 -0600

    feat(bin)!: Make `main` handle command execute
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/ricer.rs
+++ src/ricer.rs
@@ -1,21 +1,78 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: GPL-2.0-or-later WITH GPL-CC-1.0
 
-use log::{error, info};
-use std::process::ExitCode;
+//! Ricer binary implementation.
+//!
+//! Bringing everything from Ricer's core internal API together to create a
+//! functional binary.
 
-use ricer::cli::Cli;
+use anyhow::Result;
+use log::error;
+use std::ffi::OsString;
 
-fn main() -> ExitCode {
-    match Cli::new_run() {
-        Ok(out) => {
-            info!("{}", out);
-            ExitCode::SUCCESS
+use ricer_core::cli::RicerCli;
+
+/// Starting point of Ricer binary.
+///
+/// # Postconditions
+///
+/// 1. Parse and execute Ricer command set.
+/// 2. Log any errors.
+/// 3. Provide [`ExitCode`] before exiting.
+fn main() {
+    std::process::exit(
+        match run_ricer(std::env::args_os) {
+            Ok(exit_code) => exit_code,
+            Err(error) => {
+                error!("{}", error);
+                ExitCode::Failure
+            }
         }
+        .into(),
+    );
+}
 
-        Err(error) => {
-            error!("{}", error);
-            ExitCode::FAILURE
+/// Run Ricer binary.
+///
+/// # Postconditions
+///
+/// 1. Parse and execute Ricer command set.
+/// 2. Provide [`ExitCode`] after processing.
+///
+/// # Invariants
+///
+/// 1. Do not allow `main` to panic, i.e., catch all possible errors so `main`
+///    can report them.
+fn run_ricer<I, F>(args: F) -> Result<ExitCode>
+where
+    I: IntoIterator<Item = OsString>,
+    F: FnOnce() -> I + Clone,
+{
+    let opts = RicerCli::parse_args(args());
+    env_logger::Builder::new()
+        .format_timestamp(None)
+        .filter_level(opts.log_opts.log_level_filter())
+        .init();
+
+    // TODO: match and execute command in Ricer's command set...
+
+    Ok(ExitCode::Success)
+}
+
+/// General exit code status.
+enum ExitCode {
+    /// Ricer binary exited with no errors.
+    Success,
+
+    /// Ricer binary exited with errors.
+    Failure,
+}
+
+impl From<ExitCode> for i32 {
+    fn from(code: ExitCode) -> Self {
+        match code {
+            ExitCode::Success => 0,
+            ExitCode::Failure => 1,
         }
     }
 }
