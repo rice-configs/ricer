b52a313c4323ab1e36c0e05dc67dd280da3a9fa1
Jason Pena <jasonpena@awkless.com>
2024-07-11 12:45:29 -0600

    feat(ricer_core::context): Setup context for enter command
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ src/context/enter.rs
@@ -0,0 +1,33 @@
+// SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
+// SPDX-License-Identifier: GPL-2.0-or-later WITH GPL-CC-1.0
+
+use crate::cli::{CommandSet, RicerCli, RunHookOpts};
+
+/// Context for enter command.
+#[derive(Debug)]
+pub struct EnterContext {
+    /// Target repository to enter.
+    pub repo: String,
+
+    /// Action to take when executing a hook specific to this command.
+    pub run_cmd_hook: RunHookOpts,
+
+    /// Action to take when executing a repository hook.
+    pub run_repo_hook: RunHookOpts,
+}
+
+impl From<RicerCli> for EnterContext {
+    fn from(opts: RicerCli) -> Self {
+        let RicerCli {cmd_opts, cmd_set, ..} = opts;
+        let cmd_set = match cmd_set {
+            CommandSet::Enter(opts) => opts,
+            _ => unreachable!("This should never happen. The command is not 'enter'!"),
+        };
+
+        Self {
+            repo: cmd_set.repo,
+            run_cmd_hook: cmd_opts.run_cmd_hook,
+            run_repo_hook: cmd_opts.run_repo_hook,
+        }
+    }
+}
