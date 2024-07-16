5573c7000d7fd28c3ff3f3b1464536c6e6548059
Jason Pena <jasonpena@awkless.com>
2024-07-15 21:45:27 -0600

    doc(ricer_core::error): Document error module
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,12 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: GPL-2.0-or-later WITH GPL-CC-1.0
 
+//! Error system.
+//!
+//! This module contains all error types that a given module of the Ricer code
+//! base may produce. Some of these errors are meant to be recoverable, others
+//! are not. It all depends upon the API of a given module.
+
 #[derive(Debug, thiserror::Error)]
 pub enum RicerError {
     #[error("Failed to interpret configuration data")]
