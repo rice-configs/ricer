6c48b7d4f4edb1436f3af6f8cfbf6d3e051635a0
Jason Pena <jasonpena@awkless.com>
2024-07-18 13:53:35 -0600

    test(tools): Add Git repository stub handler
    I have yet to make this stub function like a real Git repository. Code for that
    will come later.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- tests/tools/stubs.rs
+++ tests/tools/stubs.rs
@@ -6,7 +6,7 @@
 // This helper module is responsible for managing stubs for integration tests
 // in Ricer.
 
-use std::fs::{metadata, set_permissions, write};
+use std::fs::{create_dir, metadata, set_permissions, write};
 use std::path::{Path, PathBuf};
 
 // Basic stub of `std::fs::File`.
@@ -227,5 +227,37 @@ impl FileStubBuilder {
         }
 
         FileStub { path: self.path, data: self.data, executable: self.executable }
+    }
+}
+
+// Basic stub of a Git repository.
+//
+// Mainly used to provide basic Git repository stubs for integration testing
+// with `FakeConfigDir`.
+#[derive(Debug)]
+pub struct GitRepoStub {
+    // Absolute path to Git repository stub.
+    path: PathBuf,
+}
+
+impl GitRepoStub {
+    // Create new Git repository stub instance.
+    //
+    // Postconditions:
+    //
+    // 1. Create Git repository in target path.
+    //
+    // Errors:
+    //
+    // Panics if it cannot create Git repository.
+    pub fn new(path: impl AsRef<Path>) -> Self {
+        // TODO: Make this stub more like a Git repo rather than an empty dir...
+        create_dir(path.as_ref()).expect("Failed to create repository");
+        Self { path: path.as_ref().to_path_buf() }
+    }
+
+    // Get path to Git repository stub.
+    pub fn as_path(&self) -> &Path {
+        self.path.as_path()
     }
 }
