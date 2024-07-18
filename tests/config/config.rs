7c2f54f6118e7f70b903f6b6c9fa4e2ea8d5b401
Jason Pena <jasonpena@awkless.com>
2024-07-18 12:00:24 -0600

    test(config): Test `ricer_core::config::Config::try_to_find_hook`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- tests/config/config.rs
+++ tests/config/config.rs
@@ -10,9 +10,36 @@ use ricer_core::error::RicerError;
 use crate::tools::fakes::FakeConfigDir;
 
 #[fixture]
+fn hook_script_fixture() -> FakeConfigDir {
+    let config_dir = FakeConfigDir::builder().hook_script("fake_hook.sh", "chmod +x file").build();
+    config_dir
+}
+
+#[fixture]
 fn ignore_file_fixture() -> FakeConfigDir {
     let config_dir = FakeConfigDir::builder().ignore_file("fake_repo", "/*").build();
     config_dir
+}
+
+#[rstest]
+fn try_to_find_hook_gives_correct_path(hook_script_fixture: FakeConfigDir) {
+    let expect = hook_script_fixture.path_to_hook_script("fake_hook.sh").as_path().to_path_buf();
+    let result = match Config::new(hook_script_fixture).try_to_find_hook("fake_hook.sh") {
+        Ok(path) => path,
+        Err(error) => panic!("Expect `try_to_find_hook` to succeed, but got: {}", error),
+    };
+
+    assert_eq!(result, expect);
+}
+
+#[rstest]
+fn try_to_find_hook_no_hook_found(hook_script_fixture: FakeConfigDir) {
+    let result = match Config::new(hook_script_fixture).try_to_find_hook("nonexistant.sh") {
+        Ok(path) => panic!("Expect `try_to_find_hook` to fail, but got: {}", path.display()),
+        Err(error) => error,
+    };
+
+    assert!(matches!(result, RicerError::ConfigError(..)));
 }
 
 #[rstest]
