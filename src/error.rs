ea13f2bd28bc408b77ab7838ed37cef37017f758
Jason Pena <jasonpena@awkless.com>
2024-07-29 15:35:57 -0600

    ref(ricer::error)!: Make error handling better
    BREAKING CHANGES: I will be modifying `RicerError` to use `From` trait so I
    can just use '?' for error handling when needed instead of manual error
    conversion through `map_err`. Also providing `RicerResult` to make simplify
    result types to further take advantage of using '?' for Ricer errors.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/error.rs
+++ src/error.rs
@@ -7,8 +7,31 @@
 //! base may produce. Some of these errors are meant to be recoverable, others
 //! are not. It all depends upon the API of a given module.
 
+use anyhow::anyhow;
+
+/// Specify result type that uses [`RicerError`] for `Err`.
+pub type RicerResult<T> = Result<T, RicerError>;
+
+/// All possible error types in Ricer's internal API.
 #[derive(Debug, thiserror::Error)]
 pub enum RicerError {
-    #[error("Failed to interpret configuration data")]
-    ConfigError(#[from] anyhow::Error),
+    /// Configuration directory located, but does not exist.
+    ///
+    /// Recoverable as the location is known, so a new configuration directory
+    /// can simply be created at that location.
+    #[error("Configuration directory located at expected path, but does not exist")]
+    NoConfigDir(#[source] anyhow::Error),
+
+    /// Issued error is not recoverable.
+    ///
+    /// Caller is either expected to pass this error up the call chain, or
+    /// panic as there is no expected way to recover from this error.
+    #[error(transparent)]
+    Unrecoverable(#[from] anyhow::Error),
+}
+
+impl From<std::io::Error> for RicerError {
+    fn from(err: std::io::Error) -> RicerError {
+        RicerError::Unrecoverable(anyhow!("{}", err))
+    }
 }
