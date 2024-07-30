431b067ba3549df30eac571cba95d1f8f3c363ca
Jason Pena <jasonpena@awkless.com>
2024-07-30 16:23:54 -0600

    test(ricer::config::locator): Test `DefaultConfigDirLocator::try_new_locate`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/config/locator.rs
+++ src/config/locator.rs
@@ -6,19 +6,18 @@
 //! Basic way to locate Ricer's configuration directory at an expected area in
 //! the user's environment. This logic remains seperate from configuration
 //! directory management logic to make it easier to change the expected location
-//! of Ricer's configuration directory at any time if need be.
-//!
-//! By default Ricer expects its configuration directory to be at
-//! `$XDG_CONFIG_HOME/ricer`, i.e., `$HOME/.config/ricer`. Thus, the
-//! [`XdgConfigDirLocator`] handler is expected to be used. However, if the
-//! location of the configuration directory needs to change for whatever reason,
-//! then simply implement a new locator with [`ConfigDirLocator`] trait.
+//! of Ricer's configuration directory at any time if need be. By default Ricer
+//! expects its configuration directory to be at `$XDG_CONFIG_HOME/ricer`, i.e.,
+//! `$HOME/.config/ricer`.
 
 use anyhow::anyhow;
-use directories::ProjectDirs;
+use directories::BaseDirs;
 use log::{debug, trace, warn};
 use std::fs::create_dir;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+
+#[cfg(test)]
+use mockall::automock;
 
 use crate::error::{RicerError, RicerResult};
 
@@ -28,41 +27,93 @@ pub trait ConfigDirLocator {
     fn config_dir(&self) -> &Path;
 }
 
-/// Configuration directory locatory following XDG base directory specification.
+/// [XDG Base Directory][xdg-base-dir-spec] representation.
 ///
-/// Attempts to locate Ricer's configuration directory at
-/// `$XDG_CONFIG_HOME/ricer` following the XDG base directory specification.
-pub struct XdgConfigDirLocator {
-    xdg_spec: ProjectDirs,
+/// [xdg-base-dir-spec]: https://wiki.archlinux.org/title/XDG_Base_Directory
+#[cfg_attr(test, automock)]
+pub trait XdgBaseDirSpec {
+    /// Absolute path of `$XDG_CONFIG_HOME`.
+    fn config_home_dir(&self) -> &Path;
 }
 
-impl XdgConfigDirLocator {
-    /// Construct new [`XdgConfigDirLocator`] that tries to locate configuration
-    /// directory and ensures that it exists.
-    ///
-    /// This method will construct a new instance of [`XdgConfigDirLocator`]
-    /// that automatically locates the expected absolute path to Ricer's
-    /// configuration directory at `$XDG_CONFIG_HOME/ricer`. It will also check
-    /// that the configuration directory exists at that expected absolute path.
+/// Default implementation of [`XdgBaseDirSpec`].
+pub struct DefaultXdgBaseDirSpec {
+    xdg_spec: BaseDirs,
+}
+
+impl DefaultXdgBaseDirSpec {
+    /// Construct new instance of default XDG Base Directory Specification
+    /// handler.
     ///
     /// # Errors
     ///
-    /// 1. Issues [`RicerError::Unrecoverable`] if expected absolute path to
-    ///    configuration directory cannot be determined in user's OS
-    ///    environment.
-    /// 2. Issues [`RicerError::NoConfigDir`] if expected absolute path to
-    ///    configuration directory can be determined, but the configuration
-    ///    directory itself does not exist at the expected location.
+    /// Issues [`RicerError::Unrecoverable`] if it cannot determine home
+    /// directory of user.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use anyhow::Result;
+    /// # fn main() -> Result<()> {
+    /// use ricer::config::locator::{DefaultXdgBaseDirSpec, XdgBaseDirSpec};
+    ///
+    /// let xdg_spec = DefaultXdgBaseDirSpec::try_new()?;
+    /// println!("{}", xdg_spec.config_home_dir().display());
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// # See
+    ///
+    /// - <https://docs.rs/directories/latest/directories/struct.BaseDirs.html#method.new>
+    pub fn try_new() -> RicerResult<Self> {
+        trace!("Locate expected path to configuration directory");
+        let xdg_spec = BaseDirs::new().ok_or(RicerError::Unrecoverable(anyhow!(
+            "Failed to locate configuration directory"
+        )))?;
+
+        Ok(Self { xdg_spec })
+    }
+}
+
+impl XdgBaseDirSpec for DefaultXdgBaseDirSpec {
+    fn config_home_dir(&self) -> &Path {
+        self.xdg_spec.config_dir()
+    }
+}
+
+/// Configuration directory locatory following XDG base directory specification.
+///
+/// Attempts to locate Ricer's configuration directory using an implementation
+/// of [`XdgBaseDirSpec`]. Expects the configuration directory to be at
+/// `$XDG_CONFIG_HOME/ricer`.
+pub struct DefaultConfigDirLocator {
+    config_dir: PathBuf,
+}
+
+impl DefaultConfigDirLocator {
+    /// Construct new default configuration directory locator.
+    ///
+    /// This method will construct a new instance of [`DefaultConfigDirLocator`]
+    /// that automatically locates the expected absolute path to Ricer's
+    /// configuration directory at `$XDG_CONFIG_HOME/ricer` using an
+    /// implementation of [`XdgBaseDirSpec`].
+    ///
+    /// # Errors
+    ///
+    /// Issues [`RicerError::NoConfigDir`] if configuration directory does not
+    /// exist at `$XDG_CONFIG_HOME/ricer`.
     ///
     /// # Examples
     ///
     /// ```no_run
     /// # use anyhow::Result;
     /// # fn main() -> Result<()> {
-    /// use ricer::config::locator::XdgConfigDirLocator;
+    /// use ricer::config::locator::{DefaultXdgBaseDirSpec, DefaultConfigDirLocator};
     /// use ricer::error::RicerError;
     ///
-    /// let locator = match XdgConfigDirLocator::try_new_locate_exists() {
+    /// let xdg_spec = DefaultXdgBaseDirSpec::try_new()?;
+    /// let locator = match DefaultConfigDirLocator::try_new_locate(&xdg_spec) {
     ///     Ok(locator) => locator,
     ///     Err(RicerError::NoConfigDir(..)) => {
     ///         // TODO: Recovery logic...
@@ -74,84 +125,50 @@ impl XdgConfigDirLocator {
     /// # }
     /// ```
     ///
-    /// # See
-    ///
-    /// - <https://docs.rs/directories/latest/directories/struct.BaseDirs.html#method.new>
-    ///
-    /// [`RicerError::Unrecoverable`]: crate::error::RicerError::Unrecoverable
     /// [`RicerError::NoConfigDir`]: crate::error::RicerError::NoConfigDir
-    pub fn try_new_locate_exists() -> RicerResult<Self> {
-        let locator = Self::try_new_locate()?;
-        if !locator.config_dir().exists() {
+    pub fn try_new_locate(xdg_spec: &dyn XdgBaseDirSpec) -> RicerResult<Self> {
+        let config_dir = xdg_spec.config_home_dir().join("ricer");
+        if !config_dir.exists() {
             return Err(RicerError::NoConfigDir(anyhow!(
                 "Expected configuration directory at '{}'",
-                locator.config_dir().display()
+                config_dir.display()
             )));
         }
 
-        debug!("Configuration directory located at '{}'", locator.config_dir().display());
-        Ok(locator)
-    }
-
-    /// Construct new [`XdgConfigDirLocator`] that tries to locate configuration
-    /// directory only.
-    ///
-    /// This method will construct a new instance of [`XdgConfigDirLocator`]
-    /// that automatically locates the expected absolute path to Ricer's
-    /// configuration directory at `$XDG_CONFIG_HOME/ricer`.
-    ///
-    /// This method is mainly used at the crate level to help implement basic
-    /// recovery logic for [`try_new_locate_exists`]. See
-    /// [`recover_xdg_config_dir_locator`] for example usage.
-    ///
-    /// # Errors
-    ///
-    /// Issues [`RicerError::Unrecoverable`] if expected absolute path to
-    /// configuration directory cannot be determined in user's OS environment.
-    ///
-    /// # See
-    ///
-    /// - <https://docs.rs/directories/latest/directories/struct.BaseDirs.html#method.new>
-    ///
-    /// [`RicerError::Unrecoverable`]: crate::error::RicerError::Unrecoverable
-    /// [`try_new_locate_exists`]: #method.try_new_locate_exists
-    pub(crate) fn try_new_locate() -> RicerResult<Self> {
-        trace!("Locate expected path to configuration directory");
-        let xdg_spec = ProjectDirs::from("com", "awkless", "ricer").ok_or(
-            RicerError::Unrecoverable(anyhow!("Failed to locate configuration directory")),
-        )?;
-
-        Ok(Self { xdg_spec })
+        debug!("Configuration directory located at '{}'", config_dir.display());
+        Ok(Self { config_dir })
     }
 }
 
-impl ConfigDirLocator for XdgConfigDirLocator {
+impl ConfigDirLocator for DefaultConfigDirLocator {
     fn config_dir(&self) -> &Path {
-        self.xdg_spec.config_dir()
+        self.config_dir.as_path()
     }
 }
 
-/// Recovery logic for [`XdgConfigDirLocator::try_new_locate_exists`] issuing
-/// a [`RicerError::NoConfigDir`] error.
+/// Recovery logic for [`DefaultConfigDirLocator`].
 ///
 /// Creates the configuration directory at expected location provided by
-/// [`XdgConfigDirLocator`] as a recovery strategy.
+/// [`XdgBaseDirSpec`] as a recovery strategy.
 ///
 /// # Errors
 ///
-/// Cannot determine absolute path to configuration directory.
+/// Cannot create the configuration directory at expected path.
 ///
 /// # Examples
 ///
 /// ```no_run
 /// # use anyhow::Result;
 /// # fn main() -> Result<()> {
-/// use ricer::config::locator::{recover_xdg_config_dir_locator, XdgConfigDirLocator};
+/// use ricer::config::locator::{
+///     recover_default_config_dir_locator, DefaultXdgBaseDirSpec, DefaultConfigDirLocator
+/// };
 /// use ricer::error::RicerError;
 ///
-/// let locator = match XdgConfigDirLocator::try_new_locate_exists() {
+/// let xdg_spec = DefaultXdgBaseDirSpec::try_new()?;
+/// let locator = match DefaultConfigDirLocator::try_new_locate(&xdg_spec) {
 ///     Ok(locator) => locator,
-///     Err(RicerError::NoConfigDir(..)) => recover_xdg_config_dir_locator()?,
+///     Err(RicerError::NoConfigDir(..)) => recover_default_config_dir_locator(&xdg_spec)?,
 ///     Err(err) => return Err(err.into()),
 /// };
 /// # Ok(())
@@ -163,9 +180,47 @@ impl ConfigDirLocator for XdgConfigDirLocator {
 /// - <https://doc.rust-lang.org/std/fs/fn.create_dir.html#errors>
 ///
 /// [`RicerError::NoConfigDir`]: crate::error::RicerError::NoConfigDir
-pub fn recover_xdg_config_dir_locator() -> RicerResult<XdgConfigDirLocator> {
-    let locator = XdgConfigDirLocator::try_new_locate()?;
-    warn!("Create configuration directory at '{}'", locator.config_dir().display());
-    create_dir(locator.config_dir())?;
+pub fn recover_default_config_dir_locator(
+    xdg_spec: &dyn XdgBaseDirSpec,
+) -> RicerResult<DefaultConfigDirLocator> {
+    let config_dir = xdg_spec.config_home_dir().join("ricer");
+
+    warn!("Creating configuration directory since it does not exist at '{}'", config_dir.display());
+    create_dir(config_dir)?;
+
+    let locator = DefaultConfigDirLocator::try_new_locate(xdg_spec)?;
     Ok(locator)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+    use rstest::{rstest, fixture};
+    use ricer_test_tools::fakes::FakeConfigDir;
+
+    #[fixture]
+    fn config_dir_fixture() -> FakeConfigDir {
+        FakeConfigDir::builder().build()
+    }
+
+    #[rstest]
+    fn try_new_locate_gives_correct_path(config_dir_fixture: FakeConfigDir) {
+        let mut mock_xdg_spec = MockXdgBaseDirSpec::new();
+        mock_xdg_spec.expect_config_home_dir().return_const(config_dir_fixture.temp_dir().to_path_buf());
+
+        let locator = DefaultConfigDirLocator::try_new_locate(&mock_xdg_spec).expect("Expect success");
+        let expect = config_dir_fixture.root_dir();
+        let result = locator.config_dir();
+        assert_eq!(expect, result);
+    }
+
+    #[test]
+    fn try_new_locate_catches_nonexistant_config_dir() {
+        let mut mock_xdg_spec = MockXdgBaseDirSpec::new();
+        mock_xdg_spec.expect_config_home_dir().return_const(PathBuf::from("nonexistant"));
+
+        let result = DefaultConfigDirLocator::try_new_locate(&mock_xdg_spec);
+        assert!(matches!(result, Err(RicerError::NoConfigDir(..))));        
+    }
 }
