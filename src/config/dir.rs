54f437e7e592ed5a3c4e2d4634483d208218af97
Jason Pena <jasonpena@awkless.com>
2024-07-31 10:34:48 -0600

    feat(ricer::config::dir): Add `try_find_hook_script` to `ConfigDirManager`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/config/dir.rs
+++ src/config/dir.rs
@@ -64,6 +64,9 @@ pub trait ConfigDirManager {
     /// Find absolute path to Git repository.
     fn try_find_git_repo(&self, repo_name: impl AsRef<str>) -> RicerResult<PathBuf>;
 
+    /// Find absolute path to hook script.
+    fn try_find_hook_script(&self, hook_name: impl AsRef<str>) -> RicerResult<PathBuf>;
+
     /// Absolute path to root/top-level configuration directory.
     fn root_dir(&self) -> &Path;
 
@@ -134,6 +137,18 @@ impl ConfigDirManager for DefaultConfigDirManager {
         }
 
         Ok(repo_path)
+    }
+
+    fn try_find_hook_script(&self, hook_name: impl AsRef<str>) -> RicerResult<PathBuf> {
+        let hook_path = self.hooks_dir.join(hook_name.as_ref());
+        if !hook_path.exists() {
+            return Err(RicerError::Unrecoverable(anyhow!(
+                "Hook script '{}' does not exist",
+                hook_path.display()
+            )));
+        }
+
+        Ok(hook_path)
     }
 
     fn root_dir(&self) -> &Path {
