47777e5352d23b572ab212ac5c15354932cc9bd5
Jason Pena <jasonpena@awkless.com>
2024-08-12 13:40:27 -0600

    feat(ricer::config::file::repos_section): Add `RepoEntryBuilder`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/config/file/repos_section.rs
+++ src/config/file/repos_section.rs
@@ -30,6 +30,14 @@
 //! _macos_, _windows_, or _any_. The `user` and `hostname` fields will make
 //! Ricer only bootstrap a repository for a specific user and host.
 
+use log::trace;
+
+/// Repository entry definition implementation.
+///
+/// # Invariants
+///
+/// 1. No field is empty.
+#[derive(Debug, Default, Eq, PartialEq)]
 pub struct RepoEntry {
     /// Name of repository. Also used to name the cloned repository in the
     /// `repos` directory.
@@ -48,6 +56,126 @@ pub struct RepoEntry {
     pub target: RepoTargetEntry,
 }
 
+impl RepoEntry {
+    /// Build new repository entry definition.
+    ///
+    /// # Postconditions
+    ///
+    /// 1. Return [`RepoEntryBuilder`].
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::file::repos_section::RepoEntry;
+    ///
+    /// let repo_builder = RepoEntry::builder("vim:");
+    /// ```
+    pub fn builder(name: impl AsRef<str>) -> RepoEntryBuilder {
+        RepoEntryBuilder::new(name)
+    }
+}
+
+/// Repository entry builder.
+///
+/// Generally exists to make repository entry definitions much easier in the
+/// future!
+///
+/// # Invariants
+///
+/// 1. No field is empty.
+#[derive(Debug, Default)]
+pub struct RepoEntryBuilder {
+    name: String,
+    branch: String,
+    remote: String,
+    url: String,
+    target: RepoTargetEntry,
+}
+
+impl RepoEntryBuilder {
+    /// Construct repository entry builder.
+    ///
+    /// # Postconditions
+    ///
+    /// 1. Return new instance of repository entry builder.
+    ///
+    /// ```no_run
+    /// use ricer::config::file::repos_section::RepoEntryBuilder;
+    ///
+    /// let repo_builder = RepoEntryBuilder::new("vim");
+    /// ```
+    pub fn new(name: impl AsRef<str>) -> Self {
+        Self {
+            name: name.as_ref().to_string(),
+            branch: Default::default(),
+            remote: Default::default(),
+            url: Default::default(),
+            target: Default::default(),
+        }
+    }
+
+    pub fn branch(mut self, branch: impl AsRef<str>) -> Self {
+        self.branch = branch.as_ref().to_string();
+        self
+    }
+
+    pub fn remote(mut self, remote: impl AsRef<str>) -> Self {
+        self.remote = remote.as_ref().to_string();
+        self
+    }
+
+    pub fn url(mut self, url: impl AsRef<str>) -> Self {
+        self.url = url.as_ref().to_string();
+        self
+    }
+
+    pub fn target(mut self, target: RepoTargetEntry) -> Self {
+        self.target = target;
+        self
+    }
+
+    /// Build new [`RepoEntry`].
+    ///
+    /// # Postconditions
+    ///
+    /// 1. Valid instance of [`RepoEntry`].
+    ///
+    /// # Invariants
+    ///
+    /// 1. No field is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::file::repos_section::RepoEntryBuilder;
+    ///
+    /// let target = RepoTargetEntryBuilder::new().build();
+    /// let repo_builder = RepoEntryBuilder::new("vim");
+    ///     .branch("master")
+    ///     .remote("origin")
+    ///     .url("https://github.com/awkless/vim.git")
+    ///     .target(target)
+    ///     .build();
+    /// ```
+    pub fn build(self) -> RepoEntry {
+        trace!("Build new repository entry definition");
+        debug_assert_ne!(self.name, String::default(), "Name of repository entry is empty");
+        debug_assert_ne!(self.branch, String::default(), "Branch of repository entry is empty");
+        debug_assert_ne!(self.remote, String::default(), "Remote of repository entry is empty");
+        debug_assert_ne!(self.url, String::default(), "URL of repository entry is empty");
+        debug_assert_ne!(self.target, RepoTargetEntry::default(), "Target of repository is empty");
+
+        RepoEntry {
+            name: self.name,
+            branch: self.branch,
+            remote: self.remote,
+            url: self.url,
+            target: self.target,
+        }
+    }
+}
+
+#[derive(Debug, Default, Eq, PartialEq)]
 pub struct RepoTargetEntry {
     /// Repository will use the user's home directory as the main working tree.
     pub home: bool,
