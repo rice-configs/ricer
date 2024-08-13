b7bc3405beae96dba07f2c7f41920736695c90e8
Jason Pena <jasonpena@awkless.com>
2024-08-12 22:34:43 -0600

    style: Thanks rustfmt
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/tests/repos_section.rs
+++ src/tests/repos_section.rs
@@ -1,10 +1,10 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: GPL-2.0-or-later WITH GPL-CC-1.0
 
-use rstest::{fixture, rstest};
-use pretty_assertions::assert_eq;
-use toml_edit::DocumentMut;
 use indoc::indoc;
+use pretty_assertions::assert_eq;
+use rstest::{fixture, rstest};
+use toml_edit::DocumentMut;
 
 use crate::config::file::repos_section::*;
 
@@ -32,7 +32,8 @@ fn toml_doc_fixture() -> DocumentMut {
 fn deserialize_full_repo_entry(toml_doc_fixture: DocumentMut) {
     let repos_table = toml_doc_fixture.get("repos").expect("The 'repos' section does not exist");
     let repos_table = repos_table.as_table().expect("The 'repos' section is not a table");
-    let repo_entry = repos_table.get_key_value("full_entry").expect("Full entry fixture does not exist");
+    let repo_entry =
+        repos_table.get_key_value("full_entry").expect("Full entry fixture does not exist");
 
     let result = RepoEntry::from(repo_entry);
     let target = RepoTargetEntry::builder()
@@ -54,7 +55,8 @@ fn deserialize_full_repo_entry(toml_doc_fixture: DocumentMut) {
 fn deserialize_repo_entry_with_missing_target_entry(toml_doc_fixture: DocumentMut) {
     let repos_table = toml_doc_fixture.get("repos").expect("The 'repos' section does not exist");
     let repos_table = repos_table.as_table().expect("The 'repos' section is not a table");
-    let repo_entry = repos_table.get_key_value("no_target_entry").expect("No target fixture does not exist");
+    let repo_entry =
+        repos_table.get_key_value("no_target_entry").expect("No target fixture does not exist");
 
     let result = RepoEntry::from(repo_entry);
     let target = RepoTargetEntry::default();
