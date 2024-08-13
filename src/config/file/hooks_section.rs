dfb6a2c385e17e62ebe93de24407b3a8c499a607
Jason Pena <jasonpena@awkless.com>
2024-08-13 10:06:29 -0600

    feat(ricer::config::file::hooks_section): Add builder for `HookEntry`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/config/file/hooks_section.rs
+++ src/config/file/hooks_section.rs
@@ -20,6 +20,8 @@
 //! be executed _after_ the target command. The `repo` command is the target
 //! repository to execute the current hook entry on only.
 
+use log::trace;
+
 /// Command hook entry definition implementation.
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct CommandHookEntry {
@@ -41,4 +43,164 @@ pub struct HookEntry {
 
     /// Execute hook script _only_ for a target repository definition.
     pub repo: Option<String>,
+}
+
+impl HookEntry {
+    /// Build a new hook entry definition.
+    ///
+    /// # Preconditions
+    ///
+    /// None.
+    ///
+    /// # Postconditions
+    ///
+    /// 1. Return hook entry builder instance.
+    ///
+    /// # Invariants
+    ///
+    /// None.
+    ///
+    /// # Side Effects
+    ///
+    /// None.
+    pub fn builder() -> HookEntryBuilder {
+        HookEntryBuilder::new()
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct HookEntryBuilder {
+    pre: Option<String>,
+    post: Option<String>,
+    repo: Option<String>,
+}
+
+impl HookEntryBuilder {
+    /// Construct new hook entry builder.
+    ///
+    /// # Preconditions
+    ///
+    /// None.
+    ///
+    /// # Postconditions
+    ///
+    /// 1. Return new instance of hook entry builder.
+    ///
+    /// # Invariants
+    ///
+    /// None.
+    ///
+    /// # Side Effects
+    ///
+    /// None.
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    /// Set pre-script to run _before_ target command.
+    ///
+    /// # Preconditions
+    ///
+    /// None.
+    ///
+    /// # Postconditions
+    ///
+    /// 1. Set [`pre`] field.
+    ///
+    /// # Invariants
+    ///
+    /// None.
+    ///
+    /// # Side Effects
+    ///
+    /// None.
+    ///
+    /// [`pre`]: #member.pre
+    pub fn pre(mut self, script_name: impl AsRef<str>) -> Self {
+        self.pre = Some(script_name.as_ref().to_string());
+        self
+    }
+
+    /// Set post-script to run _before_ target command.
+    ///
+    /// # postconditions
+    ///
+    /// None.
+    ///
+    /// # Postconditions
+    ///
+    /// 1. Set [`post`] field.
+    ///
+    /// # Invariants
+    ///
+    /// None.
+    ///
+    /// # Side Effects
+    ///
+    /// None.
+    ///
+    /// [`post`]: #member.post
+    pub fn post(mut self, script_name: impl AsRef<str>) -> Self {
+        self.post = Some(script_name.as_ref().to_string());
+        self
+    }
+
+    /// Set repo-script to run _before_ target command.
+    ///
+    /// # repoconditions
+    ///
+    /// None.
+    ///
+    /// # Postconditions
+    ///
+    /// 1. Set [`repo`] field.
+    ///
+    /// # Invariants
+    ///
+    /// None.
+    ///
+    /// # Side Effects
+    ///
+    /// None.
+    ///
+    /// [`repo`]: #member.repo
+    pub fn repo(mut self, repo_name: impl AsRef<str>) -> Self {
+        self.repo = Some(repo_name.as_ref().to_string());
+        self
+    }
+
+    /// Build new [`HookEntry`].
+    ///
+    /// # Preconditions
+    ///
+    /// None.
+    ///
+    /// # Postconditions
+    ///
+    /// 1. Return new instance of [`HookEntry`].
+    ///
+    /// # Invariants
+    ///
+    /// None.
+    ///
+    /// # Side Effects
+    ///
+    /// None.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::file::hooks_section::HookEntry;
+    ///
+    /// let hook_entry = HookEntryBuilder::new()
+    ///     .pre("hook.sh")
+    ///     .post("hook.sh")
+    ///     .repo("vim")
+    ///     .build();
+    /// println!("{:#?}", hook_entry);
+    /// ```
+    pub fn build(self) -> HookEntry {
+        trace!("Build new hook entry definition");
+        HookEntry { pre: self.pre, post: self.post, repo: self.repo }
+    }
 }
