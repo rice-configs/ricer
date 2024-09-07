9ad736aa8dc92e7cf628e367e400b95cbe0a4ba1
Jason Pena <jasonpena@awkless.com>
2024-09-07 17:58:01 -0600

    ref(ricer_test_tools)!: Give `GitRepoFixture` the ability to manage Git repos
    BREAKING CHANGES: Finally finished todo of adding Git repository handling
    capabilities to GitRepoFixture.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- crates/ricer_test_tools/src/fixtures/git.rs
+++ crates/ricer_test_tools/src/fixtures/git.rs
@@ -1,32 +1,144 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: GPL-2.0-or-later WITH GPL-CC-1.0
 
-use std::fs::create_dir;
+use git2::{IndexAddOption, Repository, RepositoryInitMode, RepositoryInitOptions};
 use std::path::{Path, PathBuf};
 
-/// Basic stub of a Git repository.
+use crate::util::err_check;
+
+/// Basic git repository fixture.
 ///
-/// Mainly used to provide basic Git repository stubs for integration testing
-/// with `FakeConfigDir`.
-#[derive(Debug)]
+/// Allows caller to create a basic git repository fixture, or a git repository
+/// that uses the _fake bare_ technique. A fake bare repository is a repository
+/// does not have a ".git" directory similar to a bare repository, but still
+/// retains a working tree that is different from the git directory itself.
 pub struct GitRepoFixture {
-    path: PathBuf,
+    git_dir: PathBuf,
+    repo: Repository,
 }
 
 impl GitRepoFixture {
-    /// Create new Git repository stub instance.
+    /// Create a new regular git repository.
     ///
-    /// Errors:
+    /// # Panics
     ///
-    /// Panics if it cannot create Git repository.
-    pub fn new(path: impl AsRef<Path>) -> Self {
-        // TODO: Make this stub more like a Git repo rather than an empty dir...
-        create_dir(path.as_ref()).expect("Failed to create repository");
-        Self { path: path.as_ref().to_path_buf() }
+    /// Will panic if git repository cannot be created and initialized.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer_test_tools::fixtures::GitRepoFixture;
+    ///
+    /// let repo = GitRepoFixture::init("vim");
+    /// ```
+    pub fn init(path: impl AsRef<Path>) -> Self {
+        let repo = err_check!(Repository::init(path.as_ref()));
+        Self { git_dir: path.as_ref().into(), repo }
     }
 
-    /// Get path to Git repository stub.
+    /// Create a new _fake bare_ git repository.
+    ///
+    /// # Panics
+    ///
+    /// Will panic if repository cannot be created with "fake bare" technique.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer_test_tools::fixtures::GitRepoFixture;
+    /// use ricer_test_tools::fakes::FakeHomeDir;
+    ///
+    /// let fake_home = FakeHomeDir::new();
+    /// let repo = GitRepoFixture::init_fake_bare(fake_home.as_path().join("vim"), fake_home.as_path());
+    /// ```
+    pub fn init_fake_bare(path: impl AsRef<Path>, work_dir: impl AsRef<Path>) -> Self {
+        let mut opts = RepositoryInitOptions::new();
+        opts.workdir_path(work_dir.as_ref());
+        opts.bare(false);
+        opts.mode(RepositoryInitMode::SHARED_UMASK);
+        opts.no_dotgit_dir(true);
+        let repo = err_check!(Repository::init_opts(path.as_ref(), &opts));
+        Self { git_dir: path.as_ref().into(), repo }
+    }
+
+
+    /// Add all currently unstaged files in repository.
+    ///
+    /// # Panics
+    ///
+    /// Will panic if any issues are encountered with adding unstaged files.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::fs::write;
+    ///
+    /// use ricer_test_tools::fixtures::GitRepoFixture;
+    /// use ricer_test_tools::fakes::FakeHomeDir;
+    /// use ricer_test_tools::util::err_check;
+    ///
+    /// let fake_home = FakeHomeDir::new();
+    /// let repo = GitRepoFixture::init_fake_bare(fake_home.as_path().join("vim"), fake_home.as_path());
+    /// err_check!(write(fake_home.as_path().join(".vimrc"), "configuring vim!"));
+    /// repo.add_all();
+    /// ```
+    pub fn add_all(&self) {
+        let mut index = err_check!(self.repo.index());
+        err_check!(index.add_all(["*"].iter(), IndexAddOption::DEFAULT, None));
+        err_check!(index.write());
+    }
+
+    /// Commit changes staged changes.
+    ///
+    /// # Panics
+    ///
+    /// Will panic if staged panics cannot be committed for whatever reason.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::fs::write;
+    ///
+    /// use ricer_test_tools::fixtures::GitRepoFixture;
+    /// use ricer_test_tools::fakes::FakeHomeDir;
+    /// use ricer_test_tools::util::err_check;
+    ///
+    /// let fake_home = FakeHomeDir::new();
+    /// let repo = GitRepoFixture::init_fake_bare(fake_home.as_path().join("vim"), fake_home.as_path());
+    /// err_check!(write(fake_home.as_path().join(".vimrc"), "configuring vim!"));
+    /// repo.add_all();
+    /// repo.commit("Initial commit");
+    /// ```
+    pub fn commit(&self, msg: impl AsRef<str>) {
+        let mut index = err_check!(self.repo.index());
+        let tree_id = err_check!(index.write_tree());
+        let sig = err_check!(self.repo.signature());
+        let mut parents = Vec::new();
+        if let Some(parent) = self.repo.head().ok().map(|h| h.target().unwrap()) {
+            parents.push(err_check!(self.repo.find_commit(parent)));
+        }
+        let parents = parents.iter().collect::<Vec<_>>();
+        err_check!(self.repo.commit(
+            Some("HEAD"),
+            &sig,
+            &sig,
+            msg.as_ref(),
+            &self.repo.find_tree(tree_id).expect("Failed to find tree"),
+            &parents,
+        ));
+    }
+
+    /// Get current hash id of HEAD.
+    ///
+    /// # Panics
+    ///
+    /// Will panic if hash id of HEAD cannot be obtained for whatever reason.
+    pub fn rev_parse_head(&self) -> String {
+        err_check!(self.repo.revparse_single("HEAD")).id().to_string()
+    }
+
+    /// Get path to git repository.
     pub fn as_path(&self) -> &Path {
-        self.path.as_path()
+        self.git_dir.as_path()
     }
 }
