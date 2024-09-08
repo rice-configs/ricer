d55b3bc656ceb6e1f2bc7079c0dae17c18c79f2a
Jason Pena <jasonpena@awkless.com>
2024-09-07 22:11:32 -0600

    ref(ricer)!: Comment out test cases
    BREAKING CHANGES: I am going to refactor massive portions of the codebase. This
    means that most of these tests I commented out are not going to work in the
    future!
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/tests.rs
+++ src/tests.rs
@@ -1,14 +1,14 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: GPL-2.0-or-later WITH GPL-CC-1.0
 
-mod default_cfg_dir_locator;
+// mod default_cfg_dir_locator;
 
-mod default_cfg_dir_mgr;
+// mod default_cfg_dir_mgr;
 
-mod default_cfg_file_mgr;
+// mod default_cfg_file_mgr;
 
-mod cli;
+// mod cli;
 
-mod repo_entry;
+// mod repo_entry;
 
-mod cmd_hook_entry;
+// mod cmd_hook_entry;
--- tests/integration/main.rs
+++ tests/integration/main.rs
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: GPL-2.0-or-later WITH GPL-CC-1.0
 
-mod config_manager;
+// mod config_manager;
 
-mod default_command_hook_manager;
+// mod default_command_hook_manager;
