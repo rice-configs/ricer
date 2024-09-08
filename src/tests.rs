3e56b7dc055d8445f39cb9e2a60e2fa3a781a367
Jason Pena <jasonpena@awkless.com>
2024-09-07 23:17:02 -0600

    ref(tests): Allow tests for `ricer::cli`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/tests.rs
+++ src/tests.rs
@@ -1,13 +1,13 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: MIT
 
+mod cli;
+
 // mod default_cfg_dir_locator;
 
 // mod default_cfg_dir_mgr;
 
 // mod default_cfg_file_mgr;
-
-// mod cli;
 
 // mod repo_entry;
 
