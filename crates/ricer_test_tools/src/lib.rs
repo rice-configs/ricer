d5c785ba8e55a20ad79efa1babd91fed58f91478
Jason Pena <jasonpena@awkless.com>
2024-09-07 21:14:41 -0600

    doc(ricer_test_tools): Describe ricer_test_tools crate in rustdocs
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- crates/ricer_test_tools/src/lib.rs
+++ crates/ricer_test_tools/src/lib.rs
@@ -1,6 +1,13 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: GPL-2.0-or-later WITH GPL-CC-1.0
 
+//! Test tooling for Ricer.
+//!
+//! This external crate provides a basic test tooling framework for testing the
+//! internal functionality of Ricer. The tools here are specific to Ricer,
+//! meaning that they are not great for general use outside of the Ricer
+//! project.
+
 pub mod fakes;
 pub mod fixtures;
 pub mod util;
