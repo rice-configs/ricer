e259737de302634cd1bd32de75d9b63b5b58922e
Jason Pena <jasonpena@awkless.com>
2024-09-13 22:41:54 -0600

    ref(ricer::config)!: Improve config entry implementation
    BREAKING CHANGES: Moved out file and directory manager code out. Will be placing
    this manager code in a separate module. The `*Entry` API and their associated
    builder patterns have been simplified with improved documentation. The API for
    each `*Entry` object has been improved to also include additional fields to
    improve existing configuration functionality in the future for the user and
    Ricer's command set as a whole.
    
    More specifically, the `RepoEntry` API has been extended to include
    bootstrapping options for the user. The `CmdHookEntry` API removes the `repo`
    field for `workdir` field to make it easier for the user to control the
    working directories of their hook scripts without the need to rely on specific
    repository definitions. This also decouples `CmdHookEntry` from `RepoEntry`,
    which will make the implementation of the repository manager and hook manager
    simpler and avoid mutability issues compared to the original design.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ src/config/hook.rs
@@ -0,0 +1,255 @@
+// SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
+// SPDX-License-Identifier: MIT
+
+use log::trace;
+use std::path::PathBuf;
+use toml_edit::visit::{visit_inline_table, Visit};
+use toml_edit::{InlineTable, Item, Key, Value, Table, Array};
+
+/// Command hook entry definition.
+///
+/// An intermediary structure to help deserialize command hook entries from
+/// Ricer's command hook configuration file.
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub struct CmdHookEntry {
+    /// Name of command to bind hook definition entries too.
+    pub cmd: String,
+
+    /// Array of hook entries to execute.
+    pub hooks: Vec<HookEntry>,
+}
+
+impl CmdHookEntry {
+    /// Construct new command hook entry definition.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::CmdHookEntry;
+    ///
+    /// let hook = CmdHookEntry::new("commit");
+    /// ```
+    pub fn new(cmd: impl Into<String>) -> Self {
+        trace!("Construct new command hook entry definition");
+        Self { cmd: cmd.into(), hooks: Default::default() }
+    }
+
+    /// Add hook entry into command hook definition.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::{CmdHookEntry, HookEntry};
+    ///
+    /// let mut cmd_hook = CmdHookEntry::new("commit");
+    /// let hook = HookEntry::builder()
+    ///     .pre("hook.sh")
+    ///     .post("hook.sh")
+    ///     .workdir("/path/to/work/dir")
+    ///     .build();
+    /// cmd_hook.add_hook(hook);
+    /// ```
+    pub fn add_hook(&mut self, hook: HookEntry) {
+        self.hooks.push(hook);
+    }
+
+    /// Serialize command hook entry definition into a TOML item.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use anyhow::Result;
+    /// # fn main() -> Result<()> {
+    /// use indoc::indoc;
+    /// use ricer::config::{CmdHookEntry, HookEntry};
+    /// use toml_edit::DocumentMut
+    ///
+    /// let mut cmd_hook = CmdHookEntry::new("commit");
+    /// cmd_hook.push(HookEntry::builder().pre("hook.sh"));
+    /// cmd_hook.push(HookEntry::builder().post("hook.sh").workdir("/some/path"));
+    /// let (key, value) = cmd_hook.to_toml();
+    /// 
+    /// let mut table_doc: DocumentMut = "[hooks]".parse()?;
+    /// let hooks_table = toml_doc.get_mut("hooks").unwrap();
+    /// let hooks_table = hooks_table.as_table_mut().unwrap();
+    /// hooks_table.insert(&key, value);
+    /// hooks_table.set_implicit(true);
+    /// let expect = indoc! {r#"
+    ///     [hooks]
+    ///     commit = [
+    ///         { pre = "hook.sh" },
+    ///         { post = "hook.sh", workdir = "/some/path" }
+    ///     ]
+    /// "#};
+    /// let result = toml_doc.to_string();
+    /// assert_eq!(expect, result);
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn to_toml(&self) -> (Key, Item) {
+        let mut tables = Array::new();
+
+        for hook in self.hooks.iter() {
+            let mut inline = InlineTable::new();
+            if let Some(pre) = &hook.pre {
+                inline.insert("pre", Value::from(pre));
+            }
+
+            if let Some(post) = &hook.post {
+                inline.insert("post", Value::from(post));
+            }
+
+            if let Some(workdir) = &hook.workdir {
+                inline.insert("workdir", Value::from(String::from(workdir.to_string_lossy())));
+            }
+            tables.push(Value::from(inline));
+        }
+
+        let key = Key::new(&self.cmd);
+        let value = Item::Value(Value::from(tables));
+        (key, value)
+    }
+}
+
+impl<'toml> From<(&'toml Key, &'toml Item)> for CmdHookEntry {
+    fn from(toml_entry: (&'toml Key, &'toml Item)) -> Self {
+        let (key, value) = toml_entry;
+        let mut hook = CmdHookEntry::new(key.get());
+        hook.visit_item(value);
+        hook
+    }
+}
+
+impl<'toml> Visit<'toml> for CmdHookEntry {
+    fn visit_inline_table(&mut self, node: &'toml InlineTable) {
+        let pre = if let Some(pre) = node.get("pre") { pre.as_str() } else { None };
+        let post = if let Some(post) = node.get("post") { post.as_str() } else { None };
+        let workdir = if let Some(workdir) = node.get("workdir") { workdir.as_str() } else { None };
+
+        let hook = HookEntry::builder();
+        let hook = if let Some(pre) = pre { hook.pre(pre) } else { hook };
+        let hook = if let Some(post) = post { hook.post(post) } else { hook };
+        let hook = if let Some(workdir) = workdir { hook.workdir(workdir) } else { hook };
+        self.add_hook(hook.build());
+
+        visit_inline_table(self, node);
+    }
+}
+
+/// Hook entry definition.
+///
+/// An intermediary structure to help deserialize hook entries for command hook
+/// definitions.
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub struct HookEntry {
+    /// Execute hook script _before_ command itself.
+    pub pre: Option<String>,
+
+    /// Execute hook script _after_ command itself.
+    pub post: Option<String>,
+
+    /// Set working directory of hook script.
+    pub workdir: Option<PathBuf>,
+}
+
+impl HookEntry {
+    /// Build new hook entry definition.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::HookEntry;
+    ///
+    /// let hook = HookEntry::builder()
+    ///     .pre("hook.sh")
+    ///     .pre("hook.sh")
+    ///     .workdir("/path/to/work/dir/")
+    ///     .build();
+    /// ```
+    pub fn builder() -> HookEntryBuilder {
+        HookEntryBuilder::new()
+    }
+}
+
+/// Builder for [`HookEntry`].
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub struct HookEntryBuilder {
+    pre: Option<String>,
+    post: Option<String>,
+    workdir: Option<PathBuf>,
+}
+
+impl HookEntryBuilder {
+    /// Construct new hook entry builder.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::HookEntryBuilder;
+    ///
+    /// let builder = HookEntryBuilder::new();
+    /// ```
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    /// Set hook to run _before_ command.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::HookEntryBuilder;
+    ///
+    /// let builder = HookEntryBuilder::new().pre("hook.sh");
+    /// ```
+    pub fn pre(mut self, script: impl Into<String>) -> Self {
+        self.pre = Some(script.into());
+        self
+    }
+
+    /// Set hook to run _after_ command.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::HookEntryBuilder;
+    ///
+    /// let builder = HookEntryBuilder::new().post("hook.sh");
+    /// ```
+    pub fn post(mut self, script: impl Into<String>) -> Self {
+        self.post = Some(script.into());
+        self
+    }
+
+    /// Set working directory for script.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::HookEntryBuilder;
+    ///
+    /// let builder = HookEntryBuilder::new().workdir("/path/to/work/dir");
+    /// ```
+    pub fn workdir(mut self, path: impl Into<PathBuf>) -> Self {
+        self.workdir = Some(path.into());
+        self
+    }
+
+    /// Build new [`HookEntry`].
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::HookEntryBuilder;
+    ///
+    /// let hook = HookEntryBuilder::new()
+    ///     .pre("hook.sh")
+    ///     .pre("hook.sh")
+    ///     .workdir("/path/to/work/dir/")
+    ///     .build();
+    /// ```
+    pub fn build(self) -> HookEntry {
+        trace!("Build new hook entry definition");
+        HookEntry { pre: self.pre, post: self.post, workdir: self.workdir }
+    }
+}
---
+++ src/config/repo.rs
@@ -0,0 +1,486 @@
+// SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
+// SPDX-License-Identifier: MIT
+
+use std::fmt;
+use toml_edit::visit::{visit_table_like_kv, Visit};
+use toml_edit::{Array, Item, Key, Table, Value};
+
+/// Repository entry definition.
+///
+/// Intermediary structure meant to help make it easier to deserialize and
+/// serialize repository configuration data to and from Ricer's repository
+/// configuration file.
+#[derive(Debug, Default, Eq, PartialEq, Clone)]
+pub struct RepoEntry {
+    /// Name of repository.
+    pub name: String,
+
+    /// Default branch.
+    pub branch: String,
+
+    /// Default remote.
+    pub remote: String,
+
+    /// Flag to determine if repository's working directory is the user's home
+    /// through _fake bare_ technique.
+    pub workdir_home: bool,
+
+    /// Bootstrap configuration for repository.
+    pub bootstrap: Option<RepoBootstrapEntry>,
+}
+
+impl RepoEntry {
+    /// Build new repository entry definition.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::RepoEntry;
+    ///
+    /// let builder = RepoEntry::builder("vim");
+    /// ```
+    ///
+    /// # See also
+    ///
+    /// - [`RepoEntryBuilder`]
+    /// - [`RepoBootstrapEntryBuilder`]
+    pub fn builder(name: impl Into<String>) -> RepoEntryBuilder {
+        RepoEntryBuilder::new(name)
+    }
+
+    /// Serialize repository entry definition into a TOML item.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use anyhow::Result;
+    /// # fn main() -> Result<()> {
+    /// use indoc::indoc;
+    /// use ricer::config::{RepoEntry, RepoBootstrapEntry, OsType};
+    /// use toml_edit::DocumentMut;
+    ///
+    /// let bootstrap = RepoBootstrapEntry::builder()
+    ///     .clone("https://github.com/awkless/vim.git")
+    ///     .os(OsType::Unix)
+    ///     .users(["awkless", "sedgwick"])
+    ///     .hosts(["lovelace", "turing"])
+    ///     .build();
+    /// let repo = RepoEntry::builder("vim")
+    ///     .branch("master")
+    ///     .remote("origin")
+    ///     .workdir_home(true)
+    ///     .bootstrap(bootstrap)
+    ///     .build();
+    /// let (key, value) = repo.to_toml();
+    ///
+    /// let mut toml_doc: DocumentMut = "[repos]".parse()?;
+    /// let repos_table = toml_doc.get_mut("repos").unwrap();
+    /// let repos_table = repos_table.as_table_mut().unwrap();
+    /// repos_table.insert(&key, value);
+    /// repos_table.set_implicit(true);
+    /// let expect = indoc! {r#"
+    ///     [repos.vim]
+    ///     branch = "master"
+    ///     remote = "origin"
+    ///     workdir_home = true
+    ///
+    ///     [repos.vim.bootstrap]
+    ///     clone = "https://github.com/awkless/vim.git"
+    ///     os = "unix"
+    ///     users = ["awkless", "sedgwick"]
+    ///     hosts = ["lovelace", "turing"]
+    /// "#};
+    /// let result = toml_doc.to_string();
+    /// assert_eq!(expect, result);
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn to_toml(&self) -> (Key, Item) {
+        let mut repo_entry = Table::new();
+        let mut bootstrap_entry = Table::new();
+
+        repo_entry.insert("branch", Item::Value(Value::from(&self.branch)));
+        repo_entry.insert("remote", Item::Value(Value::from(&self.remote)));
+        repo_entry.insert("workdir_home", Item::Value(Value::from(self.workdir_home)));
+        if let Some(bootstrap) = &self.bootstrap {
+            if let Some(clone) = &bootstrap.clone {
+                bootstrap_entry.insert("clone", Item::Value(Value::from(clone)));
+            }
+            if let Some(os) = &bootstrap.os {
+                bootstrap_entry.insert("os", Item::Value(Value::from(os.to_string())));
+            }
+            if let Some(users) = &bootstrap.users {
+                bootstrap_entry.insert("users", Item::Value(Value::Array(Array::from_iter(users))));
+            }
+            if let Some(hosts) = &bootstrap.hosts {
+                bootstrap_entry.insert("hosts", Item::Value(Value::Array(Array::from_iter(hosts))));
+            }
+            repo_entry.insert("bootstrap", Item::Table(bootstrap_entry));
+        }
+
+        let key = Key::new(&self.name);
+        let value = Item::Table(repo_entry);
+        (key, value)
+    }
+}
+
+impl<'toml> From<(&'toml Key, &'toml Item)> for RepoEntry {
+    fn from(toml_entry: (&'toml Key, &'toml Item)) -> Self {
+        let (key, value) = toml_entry;
+        let mut bootstrap = RepoBootstrapEntry::builder();
+        let mut repo = RepoEntry::builder(key.get());
+        bootstrap.visit_item(value);
+        repo.visit_item(value);
+
+        let bootstrap = bootstrap.build();
+        if !bootstrap.is_empty() {
+            repo = repo.bootstrap(bootstrap);
+        }
+        repo.build()
+    }
+}
+
+/// Builder for [`RepoEntry`].
+#[derive(Debug, Default, Eq, PartialEq, Clone)]
+pub struct RepoEntryBuilder {
+    name: String,
+    branch: String,
+    remote: String,
+    workdir_home: bool,
+    bootstrap: Option<RepoBootstrapEntry>,
+}
+
+impl RepoEntryBuilder {
+    /// Construct new repository entry definition builder.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::RepoEntryBuilder;
+    ///
+    /// let builder = RepoEntryBuilder::new("vim");
+    /// ```
+    pub fn new(name: impl Into<String>) -> Self {
+        Self {
+            name: name.into(),
+            branch: Default::default(),
+            remote: Default::default(),
+            workdir_home: Default::default(),
+            bootstrap: Default::default(),
+        }
+    }
+
+    /// Set default branch to use in repository.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::RepoEntryBuilder;
+    ///
+    /// let builder = RepoEntryBuilder::new("vim").branch("master");
+    /// ```
+    pub fn branch(mut self, branch: impl Into<String>) -> Self {
+        self.branch = branch.into();
+        self
+    }
+
+    /// Set default remote to use in repository.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::RepoEntryBuilder;
+    ///
+    /// let builder = RepoEntryBuilder::new("vim").remote("origin");
+    /// ```
+    pub fn remote(mut self, remote: impl Into<String>) -> Self {
+        self.remote = remote.into();
+        self
+    }
+
+    /// Set repository to use user's home as the main working directory.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::RepoEntryBuilder;
+    ///
+    /// let builder = RepoEntryBuilder::new("vim").workdir_home(true);
+    /// ```
+    pub fn workdir_home(mut self, choice: bool) -> Self {
+        self.workdir_home = choice;
+        self
+    }
+
+    /// Set bootstrapping options for repository.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::{RepoEntryBuilder, RepoBootstrapEntry, OsType};
+    ///
+    /// let bootstrap = RepoBootstrapEntry::builder()
+    ///     .clone("https://github.com/awkless/vim.git")
+    ///     .os(OsType::Unix)
+    ///     .users(["awkless", "sedgwick"])
+    ///     .hosts(["lovelace", "turing"])
+    ///     .build();
+    /// let builder = RepoEntryBuilder::new("vim").bootstrap(bootstrap);
+    /// ```
+    pub fn bootstrap(mut self, bootstrap: RepoBootstrapEntry) -> Self {
+        self.bootstrap = Some(bootstrap);
+        self
+    }
+
+    /// Build new [`RepoEntry`]
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::{RepoEntry, RepoBootstrapEntry, OsType};
+    ///
+    /// let bootstrap = RepoBootstrapEntry::builder()
+    ///     .clone("https://github.com/awkless/vim.git")
+    ///     .os(OsType::Unix)
+    ///     .users(["awkless", "sedgwick"])
+    ///     .hosts(["lovelace", "turing"])
+    ///     .build();
+    /// let repo = RepoEntry::builder("vim")
+    ///     .branch("master")
+    ///     .remote("origin")
+    ///     .workdir_home(true)
+    ///     .bootstrap(bootstrap)
+    ///     .build();
+    /// ```
+    pub fn build(self) -> RepoEntry {
+        RepoEntry {
+            name: self.name,
+            branch: self.branch,
+            remote: self.remote,
+            workdir_home: self.workdir_home,
+            bootstrap: self.bootstrap,
+        }
+    }
+}
+
+impl<'toml> Visit<'toml> for RepoEntryBuilder {
+    fn visit_table_like_kv(&mut self, key: &'toml str, node: &'toml Item) {
+        match key {
+            "branch" => self.branch = node.as_str().unwrap_or_default().to_string(),
+            "remote" => self.remote = node.as_str().unwrap_or_default().to_string(),
+            "workdir_home" => self.workdir_home = node.as_bool().unwrap_or_default(),
+            &_ => visit_table_like_kv(self, key, node),
+        }
+        visit_table_like_kv(self, key, node);
+    }
+}
+
+/// Repository bootstrap entry definition.
+///
+/// Specify bootstrapping options for a given repository entry definition. This
+/// structure serves the same purpose as [`RepoEntry`], i.e., help make it easy
+/// to serialize and deserialize repository configuraiton information to and
+/// from Ricer's repository configuration file.
+#[derive(Debug, Default, Eq, PartialEq, Clone)]
+pub struct RepoBootstrapEntry {
+    pub clone: Option<String>,
+    pub os: Option<OsType>,
+    pub users: Option<Vec<String>>,
+    pub hosts: Option<Vec<String>>,
+}
+
+impl RepoBootstrapEntry {
+    /// Build a new repository bootstrap definition.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::RepoBootstrapEntry;
+    ///
+    /// let builder = RepoBootstrapEntry::builder();
+    /// ```
+    pub fn builder() -> RepoBootstrapEntryBuilder {
+        RepoBootstrapEntryBuilder::new()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.clone.is_none() && self.os.is_none() && self.users.is_none() && self.hosts.is_none()
+    }
+}
+
+/// Builder for [`RepoBootstrapEntry`].
+#[derive(Debug, Default, Eq, PartialEq, Clone)]
+pub struct RepoBootstrapEntryBuilder {
+    clone: Option<String>,
+    os: Option<OsType>,
+    users: Option<Vec<String>>,
+    hosts: Option<Vec<String>>,
+}
+
+impl RepoBootstrapEntryBuilder {
+    /// Construct new repository bootstrap definition builder.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::RepoBootstrapEntryBuilder;
+    ///
+    /// let builder = RepoBootstrapEntryBuilder::new();
+    /// ```
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    /// Set URL to clone repository from.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::RepoBootstrapEntryBuilder;
+    ///
+    /// let builder = RepoBootstrapEntryBuilder::new().clone("url here");
+    /// ```
+    pub fn clone(mut self, url: impl Into<String>) -> Self {
+        self.clone = Some(url.into());
+        self
+    }
+
+    /// Set target OS to bootstrap repository for.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::{RepoBootstrapEntryBuilder, OsType};
+    ///
+    /// let builder = RepoBootstrapEntryBuilder::new().os(OsType::Unix);
+    /// ```
+    ///
+    /// # See also
+    ///
+    /// - [`OsType`]
+    pub fn os(mut self, os: OsType) -> Self {
+        self.os = Some(os);
+        self
+    }
+
+    /// Set target users to bootstrap repository to.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::{RepoBootstrapEntryBuilder, OsType};
+    ///
+    /// let builder = RepoBootstrapEntryBuilder::new().users(["awkless", "knuth", "bob"]);
+    /// ```
+    pub fn users<I, S>(mut self, users: I) -> Self
+    where
+        I: IntoIterator<Item = S>,
+        S: Into<String>,
+    {
+        self.users = Some(Vec::new());
+        for user in users {
+            self.users.get_or_insert(vec![]).push(user.into());
+        }
+        self
+    }
+
+    /// Set target hosts to bootstrap repository for.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::{RepoBootstrapEntryBuilder, OsType};
+    ///
+    /// let builder = RepoBootstrapEntryBuilder::new().hosts(["awkless", "knuth", "bob"]);
+    /// ```
+    pub fn hosts<I, S>(mut self, hosts: I) -> Self
+    where
+        I: IntoIterator<Item = S>,
+        S: Into<String>,
+    {
+        self.hosts = Some(Vec::new());
+        for host in hosts {
+            self.hosts.get_or_insert(vec![]).push(host.into());
+        }
+        self
+    }
+
+    /// Build [`RepoBootstrapEntry`].
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use ricer::config::{RepoBootstrapEntryBuilder, OsType};
+    ///
+    /// let bootstrap = RepoBootstrapEntryBuilder::new()
+    ///     .clone("https://github.com/awkless/vim.git")
+    ///     .os(OsType::Unix)
+    ///     .users(["awkless", "knuth", "goodwill"])
+    ///     .hosts(["lovelace", "dijkstra", "turing"])
+    ///     .build();
+    /// ```
+    pub fn build(self) -> RepoBootstrapEntry {
+        RepoBootstrapEntry { clone: self.clone, os: self.os, users: self.users, hosts: self.hosts }
+    }
+}
+
+impl<'toml> Visit<'toml> for RepoBootstrapEntryBuilder {
+    fn visit_table_like_kv(&mut self, key: &'toml str, node: &'toml Item) {
+        match key {
+            "clone" => self.clone = Some(node.as_str().unwrap_or_default().to_string()),
+            "os" => self.os = Some(OsType::from(node.as_str().unwrap_or_default())),
+            "hosts" => {
+                if let Some(hosts) = node.as_array() {
+                    self.hosts = Some(hosts.into_iter().map(|s| s.to_string()).collect())
+                }
+            }
+            "users" => {
+                if let Some(users) = node.as_array() {
+                    self.users = Some(users.into_iter().map(|s| s.to_string()).collect())
+                }
+            }
+            &_ => visit_table_like_kv(self, key, node),
+        }
+        visit_table_like_kv(self, key, node);
+    }
+}
+
+/// Supported operating systems for bootstrapping.
+#[derive(Debug, Default, Eq, PartialEq, Copy, Clone)]
+pub enum OsType {
+    /// Bootstrap to any operating system.
+    #[default]
+    Any,
+
+    /// Bootstrap to Unix-like system only.
+    Unix,
+
+    /// Bootstrap to MacOS system only.
+    MacOs,
+
+    /// Boostrap to Windows system only.
+    Windows,
+}
+
+impl From<&str> for OsType {
+    fn from(data: &str) -> Self {
+        match data {
+            "any" => Self::Any,
+            "unix" => Self::Unix,
+            "MacOs" => Self::MacOs,
+            "Windows" => Self::Windows,
+            &_ => Self::Any,
+        }
+    }
+}
+
+impl fmt::Display for OsType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            OsType::Any => write!(f, "any"),
+            OsType::Unix => write!(f, "unix"),
+            OsType::MacOs => write!(f, "macos"),
+            OsType::Windows => write!(f, "windows"),
+        }
+    }
+}
