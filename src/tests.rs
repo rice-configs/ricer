4386fe881234e8cb6c6b72190ff4d3bf4ff15d24
Jason Pena <jasonpena@awkless.com>
2024-09-15 00:09:38 -0600

    ref(tests): Remove tests for old unrefactored `ricer::config` API
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/tests.rs
+++ src/tests.rs
@@ -2,13 +2,3 @@
 // SPDX-License-Identifier: MIT
 
 mod cli;
-
-// mod default_cfg_dir_locator;
-
-// mod default_cfg_dir_mgr;
-
-// mod default_cfg_file_mgr;
-
-// mod repo_entry;
-
-// mod cmd_hook_entry;
