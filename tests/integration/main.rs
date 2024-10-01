811198b6920a876a9dce9af5cfdf6c020c0fd0e3
Jason Pena <jasonpena@awkless.com>
2024-09-30 22:04:01 -0600

    test(integration): Test `ConfigFile::load`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- tests/integration/main.rs
+++ tests/integration/main.rs
@@ -1,6 +1,4 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: MIT
 
-// mod config_manager;
-
-// mod default_command_hook_manager;
+mod config_file;
