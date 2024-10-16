ec94c4e400905d9f7d3ec7459db263b852436965
Jason Pena <jasonpena@awkless.com>
2024-10-15 21:59:04 -0600

    feat(bin): add `main`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
+// SPDX-License-Identifier: MIT
+
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    println!("Hello world from Ricer!");
+    Ok(())
+}
