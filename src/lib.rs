b935bf22df0d423b6c073bb53cae406a7822fda3
Jason Pena <jasonpena@awkless.com>
2024-10-17 15:50:28 -0600

    feat(bin): use `ricer::ui::Cli` in `main`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: MIT
 
-pub mod ui;
-pub mod error;
 pub mod context;
+pub mod error;
+pub mod ui;
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,37 @@
 // SPDX-License-Identifier: MIT
 
 use anyhow::Result;
+use log::error;
+use std::ffi::OsString;
 
-fn main() -> Result<()> {
-    println!("Hello world from Ricer!");
-    Ok(())
+use ricer::error::ExitCode;
+use ricer::ui::Cli;
+
+fn main() {
+    let code = match run_ricer(std::env::args_os) {
+        Ok(code) => code,
+        Err(err) => {
+            error!("{:?}", err);
+            ExitCode::Failure
+        }
+    }
+    .into();
+
+    std::process::exit(code);
+}
+
+fn run_ricer<I, F>(args: F) -> Result<ExitCode>
+where
+    I: IntoIterator<Item = OsString>,
+    F: FnOnce() -> I + Clone,
+{
+    let opts = Cli::parse_args(args());
+    env_logger::Builder::new()
+        .format_target(false)
+        .format_timestamp(None)
+        .format_indent(Some(8))
+        .filter_level(opts.log_opts.log_level_filter())
+        .init();
+
+    Ok(ExitCode::Success)
 }
