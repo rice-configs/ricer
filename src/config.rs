6f3fecd53a231827629f1691b0c4f89ed3a4d3ea
Jason Pena <jasonpena@awkless.com>
2024-10-20 16:02:43 -0600

    feat(ricer::config): implement `FromStr` for `Toml`
    The `Toml` object only parses strings. It will not perform I/O at all, because
    the caller is expected to do that if they want to parse something like a file.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,13 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: MIT
 
+use crate::error::RicerError;
+
+use anyhow::anyhow;
 use log::trace;
-use toml_edit::DocumentMut;
 use std::fmt;
+use std::str::FromStr;
+use toml_edit::DocumentMut;
 
 #[derive(Clone, Default, Debug)]
 pub struct Toml {
@@ -20,5 +24,15 @@ impl Toml {
 impl fmt::Display for Toml {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.doc)
+    }
+}
+
+impl FromStr for Toml {
+    type Err = RicerError;
+
+    fn from_str(data: &str) -> Result<Self, Self::Err> {
+        let doc: DocumentMut =
+            data.parse().map_err(|e| -> RicerError { anyhow!("{}", e).into() })?;
+        Ok(Self { doc })
     }
 }
