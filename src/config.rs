d246d28e77618fa022694518a8c33b8b693924e3
Jason Pena <jasonpena@awkless.com>
2024-10-20 15:36:06 -0600

    feat(ricer::config): add `Toml::new`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: MIT
 
+use log::trace;
 use toml_edit::DocumentMut;
 
 #[derive(Clone, Default, Debug)]
@@ -9,5 +10,8 @@ pub struct Toml {
 }
 
 impl Toml {
-    // TODO: implement this...
+    pub fn new() -> Self {
+        trace!("Construct new TOML parser");
+        Self { doc: DocumentMut::new() }
+    }
 }
