14ab44b75b7f747dc3989bf026e1dfa3175d9840
Jason Pena <jasonpena@awkless.com>
2024-10-25 14:22:35 -0600

    feat(ricer::config): implement `From<(Key, Item)>` for `CommandHook`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/config/hook.rs
+++ src/config/hook.rs
@@ -2,7 +2,8 @@
 // SPDX-License-Identifier: MIT
 
 use std::path::PathBuf;
-use toml_edit::{Key, Item};
+use toml_edit::{Key, Item, InlineTable};
+use toml_edit::visit::{Visit, visit_inline_table};
 
 /// Command hook settings.
 ///
@@ -24,6 +25,42 @@ impl CommandHook {
 
     pub fn add_hook(&mut self, hook: Hook) {
         self.hooks.push(hook);
+    }
+}
+
+fn from_toml<'toml>(entry: (&'toml Key, &'toml Item)) -> CommandHook {
+    let (key, value) = entry;
+    let mut cmd_hook = CommandHook::new(key.get());
+    cmd_hook.visit_item(value);
+    cmd_hook
+}
+
+impl<'toml> From<(&'toml Key, &'toml Item)> for CommandHook {
+    fn from(entry: (&'toml Key, &'toml Item)) -> Self {
+        from_toml(entry)
+    }
+}
+
+impl From<(Key, Item)> for CommandHook {
+    fn from(entry: (Key, Item)) -> Self {
+        let (key, value) = entry;
+        from_toml((&key, &value))
+    }
+}
+
+impl<'toml> Visit<'toml> for CommandHook {
+    fn visit_inline_table(&mut self, node: &'toml InlineTable) {
+        let pre = if let Some(pre) = node.get("pre") { pre.as_str() } else { None };
+        let post = if let Some(post) = node.get("post") { post.as_str() } else { None };
+        let workdir = if let Some(workdir) = node.get("workdir") { workdir.as_str() } else { None };
+
+        let hook = Hook::new();
+        let hook = if let Some(pre) = pre { hook.pre(pre) } else { hook };
+        let hook = if let Some(post) = post { hook.post(post) } else { hook };
+        let hook = if let Some(workdir) = workdir { hook.workdir(workdir) } else { hook };
+        self.add_hook(hook);
+
+        visit_inline_table(self, node);
     }
 }
 
