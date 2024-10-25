e27894f2720a5208319e3810ec5f59cd9ca2a8eb
Jason Pena <jasonpena@awkless.com>
2024-10-25 08:04:08 -0600

    test(ricer::config): setup test cases for `Toml::remove`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/tests/config.rs
+++ src/tests/config.rs
@@ -4,10 +4,20 @@
 use crate::config::{Toml, TomlError};
 
 use anyhow::Result;
-use indoc::indoc;
+use indoc::{formatdoc, indoc};
 use pretty_assertions::assert_eq;
-use rstest::{rstest, fixture};
+use rstest::{fixture, rstest};
 use toml_edit::{Item, Key, Value};
+
+#[fixture]
+fn toml_input() -> String {
+    String::from(indoc! {r#"
+        # this coment should remain!
+        [test]
+        foo = "hello"
+        bar = true
+    "#})
+}
 
 #[rstest]
 fn toml_parse_parses_good_formatting(
@@ -27,19 +37,19 @@ fn toml_parse_catches_bad_formatting(
 }
 
 #[rstest]
-#[case(
-    indoc! {r#"
-        [foo]
-        bar = "get this"
-    "#},
-    (Key::new("bar"), Item::Value(Value::from("get this")))
-)]
-fn toml_get_returns_entry(#[case] input: &str, #[case] expect: (Key, Item)) -> Result<()> {
-    let toml: Toml = input.parse()?;
-    let (result_key, result_value) = toml.get("foo", "bar")?;
+#[case("test", "foo", (Key::new("foo"), Item::Value(Value::from("hello"))))]
+#[case("test", "bar", (Key::new("bar"), Item::Value(Value::from(true))))]
+fn toml_get_returns_entry(
+    toml_input: String,
+    #[case] table: &str,
+    #[case] key: &str,
+    #[case] expect: (Key, Item),
+) -> Result<()> {
+    let toml: Toml = toml_input.parse()?;
+    let (result_key, result_value) = toml.get(table, key)?;
     let (expect_key, expect_value) = expect;
     assert_eq!(result_key, &expect_key);
-    assert_eq!(result_value.as_str(), expect_value.as_str());
+    assert_eq!(result_value.is_value(), expect_value.is_value());
     Ok(())
 }
 
@@ -59,30 +69,31 @@ fn toml_get_catches_errors(#[case] input: &str, #[case] expect: TomlError) -> Re
 
 #[rstest]
 #[case::add_into_table(
-    indoc! {r#"
-        # add here
-        [foo]
-    "#},
-    indoc! {r#"
-        # add here
-        [foo]
-        bar = "add this"
-    "#}
+    toml_input(),
+    "test",
+    (Key::new("baz"), Item::Value(Value::from("add this"))),
+    formatdoc! {r#"
+        {}baz = "add this"
+    "#, toml_input()}
 )]
 #[case::create_new_table(
-    indoc! {r#"
-        # add new table
-    "#},
-    indoc! {r#"
-        [foo]
-        bar = "add this"
-        # add new table
-    "#},
+    toml_input(),
+    "new_test",
+    (Key::new("baz"), Item::Value(Value::from("add this"))),
+    formatdoc! {r#"
+        {}
+        [new_test]
+        baz = "add this"
+    "#, toml_input()}
 )]
-fn toml_add_new(#[case] input: &str, #[case] expect: &str) -> Result<()> {
+fn toml_add_new_entry(
+    #[case] input: String,
+    #[case] table: &str,
+    #[case] entry: (Key, Item),
+    #[case] expect: String,
+) -> Result<()> {
     let mut toml: Toml = input.parse()?;
-    let entry = (Key::new("bar"), Item::Value(Value::from("add this")));
-    let result = toml.add("foo", entry)?;
+    let result = toml.add(table, entry)?;
     assert_eq!(toml.to_string(), expect);
     assert!(result.is_none());
     Ok(())
@@ -90,21 +101,25 @@ fn toml_add_new(#[case] input: &str, #[case] expect: &str) -> Result<()> {
 
 #[rstest]
 #[case(
-    indoc! {r#"
-        # replace bar value
-        [foo]
-        bar = "this exist"
-    "#},
-    indoc! {r#"
-        # replace bar value
-        [foo]
-        bar = "replaced"
-    "#}
+    toml_input(),
+    "test",
+    (Key::new("foo"), Item::Value(Value::from("replaced"))),
+    toml_input().replace(r#"foo = "hello""#, r#"foo = "replaced""#)
 )]
-fn toml_add_replace(#[case] input: &str, #[case] expect: &str) -> Result<()> {
+#[case(
+    toml_input(),
+    "test",
+    (Key::new("bar"), Item::Value(Value::from(false))),
+    toml_input().replace(r#"bar = true"#, r#"bar = false"#)
+)]
+fn toml_add_replace_entry(
+    #[case] input: String,
+    #[case] table: &str,
+    #[case] entry: (Key, Item),
+    #[case] expect: String,
+) -> Result<()> {
     let mut toml: Toml = input.parse()?;
-    let entry = (Key::new("bar"), Item::Value(Value::from("replaced")));
-    let result = toml.add("foo", entry)?;
+    let result = toml.add(table, entry)?;
     assert_eq!(toml.to_string(), expect);
     assert!(result.is_some());
     Ok(())
@@ -116,6 +131,51 @@ fn toml_add_catches_errors(#[case] input: &str, #[case] expect: TomlError) -> Re
     let mut toml: Toml = input.parse()?;
     let stub = (Key::new("fail"), Item::Value(Value::from("this")));
     let result = toml.add("foo", stub);
+    assert_eq!(result.unwrap_err(), expect);
+    Ok(())
+}
+
+#[rstest]
+#[case(
+    toml_input(),
+    "test",
+    "foo",
+    (Key::new("foo"), Item::Value(Value::from("world"))),
+    toml_input().replace(r#"foo = "hello""#, ""),
+)]
+#[case(
+    toml_input(),
+    "test",
+    "foo",
+    (Key::new("bar"), Item::Value(Value::from(true))),
+    toml_input().replace(r#"bar = true"#, ""),
+)]
+fn toml_remove_returns_entry(
+    #[case] input: String,
+    #[case] table: &str,
+    #[case] key: &str,
+    #[case] expect: (Key, Item),
+    #[case] output: String,
+) -> Result<()> {
+    let mut toml: Toml = input.parse()?;
+    let (return_key, return_value) = toml.remove(table, key)?;
+    let (expect_key, expect_value) = expect;
+    assert_eq!(toml.to_string(), output);
+    assert_eq!(return_key, expect_key);
+    assert_eq!(return_value.is_value(), expect_value.is_value());
+    Ok(())
+}
+
+#[rstest]
+#[case::table_not_found("bar = 'foo not here'", TomlError::TableNotFound { table: "foo".into() })]
+#[case::not_table("foo = 'not a table'", TomlError::NotTable { table: "foo".into() })]
+#[case::entry_not_found(
+    "[foo] # bar not here",
+    TomlError::EntryNotFound { table: "foo".into(), key: "bar".into() }
+)]
+fn toml_remove_catches_errors(#[case] input: &str, #[case] expect: TomlError) -> Result<()> {
+    let toml: Toml = input.parse()?;
+    let result = toml.get("foo", "bar");
     assert_eq!(result.unwrap_err(), expect);
     Ok(())
 }
