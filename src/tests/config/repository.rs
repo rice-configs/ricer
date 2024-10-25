aaae5a9b95dbe9e07c03074393a87f49ad4d06ee
Jason Pena <jasonpena@awkless.com>
2024-10-25 13:24:18 -0600

    test(ricer::config): test serialization of `Repository::to_toml`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ src/tests/config/repository.rs
@@ -0,0 +1,64 @@
+// SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
+// SPDX-License-Identifier: MIT
+
+use crate::config::{Bootstrap, OsType, Repository};
+
+use anyhow::Result;
+use indoc::indoc;
+use pretty_assertions::assert_eq;
+use rstest::rstest;
+use toml_edit::{DocumentMut, Item, Key};
+
+fn setup_toml_doc(entry: (Key, Item)) -> Result<DocumentMut> {
+    let mut doc: DocumentMut = "[repos]".parse()?;
+    let table = doc.get_mut("repos").unwrap();
+    let table = table.as_table_mut().unwrap();
+    let (key, item) = entry;
+    table.insert_formatted(&key, item);
+    table.set_implicit(true);
+    Ok(doc)
+}
+
+#[rstest]
+#[case::no_bootstrap(
+    Repository::new("vim")
+        .branch("master")
+        .remote("origin")
+        .workdir_home(true),
+    indoc! {r#"
+        [repos.vim]
+        branch = "master"
+        remote = "origin"
+        workdir_home = true
+    "#}
+)]
+#[case::no_bootstrap(
+    Repository::new("vim")
+        .branch("master")
+        .remote("origin")
+        .workdir_home(true)
+        .bootstrap(
+            Bootstrap::new()
+                .clone("https://github.com/awkless/vim.git")
+                .os(OsType::Unix)
+                .users(["awkless", "sedgwick"])
+                .hosts(["lovelace", "turing"])
+        ),
+    indoc! {r#"
+        [repos.vim]
+        branch = "master"
+        remote = "origin"
+        workdir_home = true
+
+        [repos.vim.bootstrap]
+        clone = "https://github.com/awkless/vim.git"
+        os = "unix"
+        users = ["awkless", "sedgwick"]
+        hosts = ["lovelace", "turing"]
+    "#}
+)]
+fn to_toml_serializes(#[case] repo: Repository, #[case] expect: &str) -> Result<()> {
+    let doc = setup_toml_doc(repo.to_toml())?;
+    assert_eq!(doc.to_string(), expect);
+    Ok(())
+}
