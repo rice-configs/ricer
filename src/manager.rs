83823e77bb054ccd4251bab592d87a5593673301
Jason Pena <jasonpena@awkless.com>
2024-10-27 12:22:08 -0600

    feat(ricer::manager): implement `TomlManager` for `RepositoryData`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/manager.rs
+++ src/manager.rs
@@ -20,8 +20,6 @@ use mockall::automock;
 /// # See also
 ///
 /// - [`Toml`]
-///
-/// [`Toml`]: crate::config::Toml
 #[cfg_attr(test, automock(type Entry = Repository;))]
 pub trait TomlManager {
     type Entry;
@@ -30,4 +28,45 @@ pub trait TomlManager {
     fn add(&self, doc: &mut Toml, entry: Self::Entry) -> Result<Option<Self::Entry>, TomlError>;
     fn remove(&self, doc: &mut Toml, key: &str) -> Result<Self::Entry, TomlError>;
     fn rename(&self, doc: &mut Toml, from: &str, to: &str) -> Result<Self::Entry, TomlError>;
+}
+
+/// Repository data configuration management.
+///
+/// Handles serialization and deserialization of repository settings.
+/// Repository settings are held within the "repos" section of a
+/// configuration file.
+///
+/// # Invariants
+///
+/// Will preserve existing formatting of configuration file if any.
+///
+/// # See also
+///
+/// - [`Toml`]
+/// - [`Repository`]
+#[derive(Clone, Debug, Default, PartialEq, Eq)]
+pub struct RepositoryData;
+
+impl TomlManager for RepositoryData {
+    type Entry = Repository;
+
+    fn get(&self, doc: &Toml, key: &str) -> Result<Self::Entry, TomlError> {
+        let entry = doc.get("repos", key.as_ref())?;
+        Ok(Repository::from(entry))
+    }
+
+    fn add(&self, doc: &mut Toml, entry: Self::Entry) -> Result<Option<Self::Entry>, TomlError> {
+        let entry = doc.add("repos", entry.to_toml())?.map(Repository::from);
+        Ok(entry)
+    }
+
+    fn remove(&self, doc: &mut Toml, key: &str) -> Result<Self::Entry, TomlError> {
+        let entry = doc.remove("repos", key.as_ref())?;
+        Ok(Repository::from(entry))
+    }
+
+    fn rename(&self, doc: &mut Toml, from: &str, to: &str) -> Result<Self::Entry, TomlError> {
+        let entry = doc.rename("repos", from.as_ref(), to.as_ref())?;
+        Ok(Repository::from(entry))
+    }
 }
