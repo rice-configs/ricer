25ff42637ea6ec533b82f09515db3d341fe11ff7
Jason Pena <jasonpena@awkless.com>
2024-10-27 11:52:24 -0600

    feat(ricer::manager): add `DefaultDirLocator`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/manager/locator.rs
+++ src/manager/locator.rs
@@ -2,13 +2,63 @@
 // SPDX-License-Identifier: MIT
 
 use directories::ProjectDirs;
-use log::trace;
-use std::path::Path;
+use log::{debug, trace};
+use std::path::{PathBuf, Path};
 
 #[cfg(test)]
 use mockall::automock;
 
 use crate::manager::LocatorError;
+
+/// Configuration directory locator.
+///
+/// Locates absolute paths to the three special directories that Ricer
+/// relies on.
+#[cfg_attr(test, automock)]
+pub trait DirLocator {
+    /// Expected absolute path to configuration file directory.
+    fn config_dir(&self) -> &Path;
+
+    /// Expected absolute path to hook script directory.
+    fn hooks_dir(&self) -> &Path;
+
+    /// Expected absolute path to repository directory.
+    fn repos_dir(&self) -> &Path;
+}
+
+pub struct DefaultDirLocator {
+    config_dir: PathBuf,
+    hooks_dir: PathBuf,
+    repos_dir: PathBuf,
+}
+
+impl DefaultDirLocator {
+    pub fn new_locate(layout: &impl DirLayout) -> Self {
+        trace!("Construct configuration directory locator");
+        let config_dir = layout.behavior_dir().join("ricer");
+        let hooks_dir = config_dir.join("hooks");
+        let repos_dir = layout.repo_dir().join("ricer");
+
+        debug!("Configuration directory located at '{}'", config_dir.display());
+        debug!("Hook script directory located at '{}'", hooks_dir.display());
+        debug!("Repository directory located at '{}'", repos_dir.display());
+        Self { config_dir, hooks_dir, repos_dir }
+    }
+}
+
+impl DirLocator for DefaultDirLocator {
+    fn config_dir(&self) -> &Path {
+        self.config_dir.as_path()
+    }
+
+    fn hooks_dir(&self) -> &Path {
+        self.hooks_dir.as_path()
+    }
+
+    fn repos_dir(&self) -> &Path {
+        self.repos_dir.as_path()
+    }
+}
 
 /// Handle different configuration directory layouts.
 ///
