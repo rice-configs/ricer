076c87174591644d2a06bf2c206b07ded07c948c
Jason Pena <jasonpena@awkless.com>
2024-10-27 17:56:41 -0600

    feat(ricer::manager): add `ConfigManagerError`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/manager/error.rs
+++ src/manager/error.rs
@@ -1,8 +1,27 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: MIT
 
+use crate::config;
+use std::io;
+use std::path::PathBuf;
+
 #[derive(Debug, PartialEq, Eq, thiserror::Error)]
 pub enum LocatorError {
     #[error("Cannot determine path to home directory")]
     NoWayHome,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ConfigManagerError {
+    #[error("Failed to open '{path}' because '{source}'")]
+    FileOpen { source: io::Error, path: PathBuf },
+
+    #[error("Failed to read '{path}' because '{source}'")]
+    FileRead { source: io::Error, path: PathBuf },
+
+    #[error("Failed to parse '{path}' because '{source}'")]
+    Toml {
+        source: config::TomlError,
+        path: PathBuf,
+    },
 }
