d2d96028a798d73b5bb726427fbd0962ba92973b
Jason Pena <jasonpena@awkless.com>
2024-10-27 12:16:08 -0600

    feat(ricer::manager): add `TomlManager` trait
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/manager.rs
+++ src/manager.rs
@@ -7,3 +7,27 @@ mod locator;
 #[doc(inline)]
 pub use error::*;
 pub use locator::*;
+
+use crate::config::{Toml, Repository, TomlError};
+
+#[cfg(test)]
+use mockall::automock;
+
+/// TOML serialization and deserialization manager.
+///
+/// Interface to simplify serialization and deserialization of parsed TOML data.
+///
+/// # See also
+///
+/// - [`Toml`]
+///
+/// [`Toml`]: crate::config::Toml
+#[cfg_attr(test, automock(type Entry = Repository;))]
+pub trait TomlManager {
+    type Entry;
+
+    fn get(&self, doc: &Toml, key: &str) -> Result<Self::Entry, TomlError>;
+    fn add(&self, doc: &mut Toml, entry: Self::Entry) -> Result<Option<Self::Entry>, TomlError>;
+    fn remove(&self, doc: &mut Toml, key: &str) -> Result<Self::Entry, TomlError>;
+    fn rename(&self, doc: &mut Toml, from: &str, to: &str) -> Result<Self::Entry, TomlError>;
+}
