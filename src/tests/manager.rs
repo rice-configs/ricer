8a087766cf9bb89d5a87366e7242772c01b2354f
Jason Pena <jasonpena@awkless.com>
2024-10-30 15:35:45 -0600

    test(ricer::manager): test `ConfigManager::get`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/tests/manager.rs
+++ src/tests/manager.rs
@@ -1,16 +1,16 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: MIT
 
+use crate::config::{CommandHook, Entry, Hook, Repository};
 use crate::manager::{
     CommandHookData, ConfigManager, ConfigManagerError, MockDirLocator, RepositoryData, TomlManager,
 };
-use crate::config::{Repository, CommandHook, Entry, Hook};
 use crate::tests::FakeConfigDir;
 
 use anyhow::Result;
+use indoc::indoc;
 use pretty_assertions::assert_eq;
 use rstest::rstest;
-use indoc::indoc;
 
 #[rstest]
 #[case::repo_data(
@@ -125,8 +125,73 @@ fn config_manager_save_creates_new_file(
     let mut locator = MockDirLocator::new();
     locator.expect_config_dir().return_const(config_data.config_dir().into());
     let mut config = ConfigManager::load(config_type, locator)?;
-    let result = config.save();
-    assert!(result.is_ok());
+    config.save()?;
     assert!(config.location().exists());
+    Ok(())
+}
+
+#[rstest]
+#[case::repo_data(
+    RepositoryData,
+    FakeConfigDir::builder()?.config_file(
+        "repos.toml",
+        indoc! {r#"
+            [repos.vim]
+            branch = "master"
+            remote = "origin"
+            workdir_home = true
+        "#},
+    )?.build(),
+    "vim",
+    Repository::new("vim").branch("master").remote("origin").workdir_home(true),
+)]
+#[case::hook_data(
+    CommandHookData,
+    FakeConfigDir::builder()?.config_file(
+        "hooks.toml",
+        indoc! {r#"
+            [hooks]
+            commit = [
+                { pre = "hook.sh", post = "hook.sh", workdir = "/some/dir" },
+                { pre = "hook.sh" }
+            ]
+        "#},
+    )?.build(),
+    "commit",
+    CommandHook::new("commit")
+        .add_hook(Hook::new().pre("hook.sh").post("hook.sh").workdir("/some/dir"))
+        .add_hook(Hook::new().pre("hook.sh")),
+
+)]
+fn config_manager_get_works<E, T>(
+    #[case] config_type: T,
+    #[case] config_data: FakeConfigDir,
+    #[case] key: &str,
+    #[case] expect: E,
+) -> Result<()>
+where
+    E: Entry,
+    T: TomlManager<ConfigEntry = E>,
+{
+    let mut locator = MockDirLocator::new();
+    locator.expect_config_dir().return_const(config_data.config_dir().into());
+    let config = ConfigManager::load(config_type, locator)?;
+    let result = config.get(key)?;
+    assert_eq!(result, expect);
+    Ok(())
+}
+
+#[rstest]
+#[case(RepositoryData, FakeConfigDir::builder()?.build())]
+#[case(CommandHookData, FakeConfigDir::builder()?.build())]
+fn config_manager_get_catches_errors(
+    #[case] config_type: impl TomlManager,
+    #[case] config_data: FakeConfigDir,
+) -> Result<()> {
+    let mut locator = MockDirLocator::new();
+    locator.expect_config_dir().return_const(config_data.config_dir().into());
+    let config = ConfigManager::load(config_type, locator)?;
+    let result = config.get("non-existent");
+    assert!(matches!(result.unwrap_err(), ConfigManagerError::Toml { .. }));
     Ok(())
 }
