671be37885a39d3a1c2fe3a178f9aa1ca5a3acac
Jason Pena <jasonpena@awkless.com>
2024-11-04 07:22:51 -0700

    refactor(ricer::manager)!: simplify error messages
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/manager/error.rs
+++ src/manager/error.rs
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: MIT
 
 use crate::config;
+use crate::wizard;
+
 use std::io;
 use std::path::PathBuf;
 
@@ -13,18 +15,60 @@ pub enum LocatorError {
 
 #[derive(Debug, thiserror::Error)]
 pub enum ConfigManagerError {
-    #[error("Failed to make parent directory '{path}' because '{source}'")]
+    #[error("Failed to make parent directory '{path}'")]
     MakeDirP { source: io::Error, path: PathBuf },
 
-    #[error("Failed to open '{path}' because '{source}'")]
+    #[error("Failed to open '{path}'")]
     FileOpen { source: io::Error, path: PathBuf },
 
-    #[error("Failed to read '{path}' because '{source}'")]
+    #[error("Failed to read '{path}'")]
     FileRead { source: io::Error, path: PathBuf },
 
-    #[error("Failed to write '{path}' because '{source}'")]
+    #[error("Failed to write '{path}'")]
     FileWrite { source: io::Error, path: PathBuf },
 
-    #[error("Failed to parse '{path}' because '{source}'")]
+    #[error("Failed to parse '{path}'")]
     Toml { source: config::TomlError, path: PathBuf },
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum CommandHookManagerError {
+    #[error("Failed to load command hook configuration file")]
+    LoadConfig { source: ConfigManagerError },
+
+    #[error("Failed to get command hook data")]
+    GetCmdHook { source: config::TomlError },
+
+    #[error("Failed to read hook '{path}'")]
+    HookRead { source: io::Error, path: PathBuf },
+
+    #[error("Failed to run hook")]
+    RunHook { source: run_script::ScriptError },
+
+    #[error("Failed to run pager")]
+    HookPager { source: wizard::HookPagerError },
+}
+
+impl From<ConfigManagerError> for CommandHookManagerError {
+    fn from(err: ConfigManagerError) -> Self {
+        CommandHookManagerError::LoadConfig { source: err }
+    }
+}
+
+impl From<config::TomlError> for CommandHookManagerError {
+    fn from(err: config::TomlError) -> Self {
+        CommandHookManagerError::GetCmdHook { source: err }
+    }
+}
+
+impl From<run_script::ScriptError> for CommandHookManagerError {
+    fn from(err: run_script::ScriptError) -> Self {
+        CommandHookManagerError::RunHook { source: err }
+    }
+}
+
+impl From<wizard::HookPagerError> for CommandHookManagerError {
+    fn from(err: wizard::HookPagerError) -> Self {
+        CommandHookManagerError::HookPager { source: err }
+    }
 }
