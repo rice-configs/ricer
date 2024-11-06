06e82efd175b2bbae35cf8b8790f9a42937b5142
Jason Pena <jasonpena@awkless.com>
2024-11-06 09:21:44 -0700

    refactor(ricer::cli): simplify module structure
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/cli/error.rs
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
-// SPDX-License-Identifier: MIT
-
-#[derive(Debug, thiserror::Error)]
-pub enum CliError {
-    #[error("Failed to parse CLI arguments")]
-    BadParse { source: clap::Error },
-}
