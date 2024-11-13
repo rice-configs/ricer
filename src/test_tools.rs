fb8c0960e81d37d21f1fcd99daa5e6513ff19c07
Jason Pena <jasonpena@awkless.com>
2024-11-12 21:56:06 -0700

    feat(ricer::test_tools): use `err_check` instead of `anyhow::Result`
    The `err_check` macro is meant to replace the usage of `anyhow::Result` so any
    errors encountered will result in a panic with a more descriptive error message.
    This will hopefully simplify the design of unit test cases.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/test_tools.rs
+++ src/test_tools.rs
@@ -1,16 +1,38 @@
 // SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
 // SPDX-License-Identifier: MIT
 
-use anyhow::{anyhow, Result};
 use is_executable::IsExecutable;
 use mkdirp::mkdirp;
 use std::{
     collections::HashMap,
     fs::{metadata, read_to_string, set_permissions, write},
+    fmt::Write,
     path::{Path, PathBuf},
 };
 use tempfile::{Builder as TempFileBuilder, TempDir};
 use walkdir::WalkDir;
+
+#[macro_export]
+macro_rules! err_check {
+    ($expr:expr) => {
+        match $expr {
+            Ok(data) => data,
+            Err(err) => {
+                $crate::test_tools::anyhow_panic(&format!("Failed running {}", stringify!($expr)), err)
+            }
+        }
+    };
+}
+
+#[track_caller]
+pub fn anyhow_panic(what: &str, err: impl Into<anyhow::Error>) -> ! {
+    let err = err.into();
+    let mut result = format!("{}\nerror: {}\n", what, err);
+    for cause in err.chain().skip(1) {
+        let _ = writeln!(result, "Cause by: {}", cause);
+    }
+    panic!("\n{}", result);
+}
 
 /// Directory of file fixtures.
 ///
@@ -25,12 +47,12 @@ pub struct DirFixture {
 impl DirFixture {
     /// Open new directory fixture.
     ///
-    /// # Errors
+    /// # Panics
     ///
     /// - May fail if temporary directory cannot be created.
-    pub fn open() -> Result<Self> {
-        let dir = TempFileBuilder::new().tempdir()?;
-        Ok(Self { dir, fixtures: HashMap::new() })
+    pub fn open() -> Self {
+        let dir = err_check!(TempFileBuilder::new().tempdir());
+        Self { dir, fixtures: HashMap::new() }
     }
 
     pub fn with_file(
@@ -46,57 +68,57 @@ impl DirFixture {
         self
     }
 
-    /// Get tracked file fixture.
-    ///
-    /// # Errors
-    ///
-    /// - May fail if file fixture is not being tracked.
-    pub fn get_fixture(&self, path: impl AsRef<Path>) -> Result<&FileFixture> {
-        self.fixtures.get(&self.dir.path().join(path.as_ref())).ok_or(anyhow!(
-            "Fixture '{}' not being tracked in '{}'",
-            path.as_ref().display(),
-            self.dir.path().display()
-        ))
-    }
-
     /// Write all file fixtures into directory fixture.
     ///
-    /// # Errors
+    /// # Panics
     ///
     /// - May fail if tracked fixture cannot be written for whatever reason.
     ///
     /// # See also
     ///
     /// - [`FileFixture::write`]
-    pub fn write(&self) -> Result<()> {
+    pub fn write(self) -> Self {
         for (_, fixture) in self.fixtures.iter() {
-            fixture.write()?;
+            fixture.write()
         }
+        self
+    }
 
-        Ok(())
+    /// Get tracked file fixture.
+    ///
+    /// # Errors
+    ///
+    /// - May fail if file fixture is not being tracked.
+    pub fn get_fixture(&self, path: impl AsRef<Path>) -> &FileFixture {
+        match self.fixtures.get(&self.dir.path().join(path.as_ref())) {
+            Some(fixture) => fixture,
+            None => panic!("Fixture '{}' not being tracked", path.as_ref().display()),
+        }
     }
 
     /// Synchronize all file fixtures in directory fixture.
     ///
     /// Will track any files that were newly added into the temporary directory.
     ///
-    /// # Errors
+    /// # Panics
     ///
-    /// - May file if tracked file fixture cannot be syncronized for whatever
+    /// - May fail if tracked file fixture cannot be syncronized for whatever
     ///   reason.
+    /// - May fail if newly detected files in directory fixture cannot be tracked
+    ///   for whatever reason.
     ///
     /// # See also
     ///
     /// - [`FileFixture::sync`]
-    pub fn sync(&mut self) -> Result<()> {
+    pub fn sync(&mut self) {
         for (_, fixture) in self.fixtures.iter_mut() {
-            fixture.sync()?;
+            fixture.sync();
         }
 
         // Track any new files that were added by some external process(es)...
         for entry in WalkDir::new(self.dir.path()) {
-            let path = entry?.path().to_path_buf();
-            let data = read_to_string(&path)?;
+            let path = err_check!(entry).path().to_path_buf();
+            let data = err_check!(read_to_string(&path));
             let kind = match path.is_executable() {
                 true => FileFixtureKind::Script,
                 false => FileFixtureKind::Normal,
@@ -106,8 +128,6 @@ impl DirFixture {
                 .with_kind(kind);
             self.fixtures.insert(fixture.as_path().into(), fixture);
         }
-
-        Ok(())
     }
 
     pub fn as_path(&self) -> &Path {
@@ -152,27 +172,25 @@ impl FileFixture {
     /// Writes file fixture at tracked path with executable permissions set.
     /// Will make parent directory if missing for a given path.
     ///
-    /// # Errors
+    /// # Panics
     ///
     /// - May fail if parent directory cannot be made if needed.
     /// - May fail if flie cannot be written.
     /// - May fail if executable permissions cannot be set.
-    pub fn write(&self) -> Result<()> {
-        mkdirp(self.path.parent().unwrap())?;
-        write(&self.path, &self.data)?;
+    pub fn write(&self) {
+        err_check!(mkdirp(self.path.parent().unwrap()));
+        err_check!(write(&self.path, &self.data));
 
         #[cfg(unix)]
         if self.kind == FileFixtureKind::Script {
             use std::os::unix::fs::PermissionsExt;
 
-            let metadata = metadata(&self.path)?;
+            let metadata = err_check!(metadata(&self.path));
             let mut perms = metadata.permissions();
             let mode = perms.mode();
             perms.set_mode(mode | 0o111);
-            set_permissions(&self.path, perms)?;
+            err_check!(set_permissions(&self.path, perms));
         }
-
-        Ok(())
     }
 
     pub fn as_path(&self) -> &Path {
@@ -189,12 +207,11 @@ impl FileFixture {
 
     /// Synchronize file fixture at tracked path.
     ///
-    /// # Errors
+    /// # Panics
     ///
     /// Will fail if file cannot be synced, i.e., read into string form.
-    pub fn sync(&mut self) -> Result<()> {
-        self.data = read_to_string(&self.path)?;
-        Ok(())
+    pub fn sync(&mut self) {
+        self.data = err_check!(read_to_string(&self.path));
     }
 }
 
